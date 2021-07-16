d5ab973d347deaeaa7f44b8c2e945fc33608ee6e
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2021-07-16 16:44:03 +0000

    Sync LeetCode submission - Valid Mountain Array (cpp)

--- problems/valid_mountain_array/solution.cpp
+++ problems/valid_mountain_array/solution.cpp
@@ -2,11 +2,10 @@ class Solution {
 public:
     bool validMountainArray(vector<int>& arr) {
         
-        int n=arr.size(), i=0, j=n-1;
-        while(i < n-1 && arr[i+1] > arr[i]) i++;
-        while(j > 0 && arr[j-1] > arr[j]) j--;
-        return i > 0 && i==j && j < n-1;
+        int n = arr.size(), i = 0, j = n - 1;
+        while (i + 1 < n && arr[i] < arr[i + 1]) i++;
+        while (j > 0 && arr[j - 1] > arr[j]) j--;
+        return i > 0 && i == j && j < n - 1;
         
     }
 };
-
