c76391aca074777d865e1e2fc3f0ace1047aecc1
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2021-07-17 07:21:36 +0000

    Sync LeetCode submission - Missing Number (cpp)

---
+++ problems/missing_number/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int a=0,b=0;
+        for(int i=1;i<=nums.size();i++){
+            a^=i;
+        }
+        for(int i=0;i<nums.size();i++){
+            b^=nums[i];
+        }
+        return a^b;
+    }
+};
