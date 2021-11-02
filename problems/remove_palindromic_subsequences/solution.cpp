982eae49bc51dc37b44cedf033d73785ad2aa98f
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2021-11-02 02:07:37 +0000

    Sync LeetCode submission - Remove Palindromic Subsequences (cpp)

---
+++ problems/remove_palindromic_subsequences/solution.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int removePalindromeSub(string s) {
+        string str=s;
+        reverse(str.begin(), str.end());
+        if(str==s) return 1;
+        else return 2;
+    }
+};
