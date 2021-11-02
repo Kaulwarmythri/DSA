3d86e17d28c1744d91d32786b7a450ec68e94a55
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2021-11-02 17:02:04 +0000

    Sync LeetCode submission - Roman to Integer (cpp)

--- problems/roman_to_integer/solution.cpp
+++ problems/roman_to_integer/solution.cpp
@@ -1,29 +1,21 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        map<char, int> M = {
-            {'I', 1},
-            {'V', 5},
-            {'X', 10},
-            {'L', 50},
-            {'C', 100},
-            {'D', 500},
-            {'M', 1000}
-            
-        };
+        unordered_map<char, int> m;
+        m.insert(make_pair('I', 1));
+        m.insert(make_pair('V', 5));
+        m.insert(make_pair('X', 10));
+        m.insert(make_pair('L', 50));
+        m.insert(make_pair('C', 100));
+        m.insert(make_pair('D', 500));
+        m.insert(make_pair('M', 1000));
         
-        int sum = M[s.back()];
-        int i;
-        
-        for(i=s.length()-2;i>=0;--i){
-            
-            if(M[s[i]] < M[s[i+1]]){
-                sum -= M[s[i]];
-            }else{
-                sum +=  M[s[i]];
-            }
+        int i=s.back(), ans=m[s.back()];
+        for(int i = s.length()-2;i>=0;i--){
+            if(m[s[i]]>=m[s[i+1]]) ans+=m[s[i]];
+            else ans-=m[s[i]];
         }
+        return ans;
         
-        return sum;
     }
 };
