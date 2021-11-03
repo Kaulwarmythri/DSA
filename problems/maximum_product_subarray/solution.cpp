6ed8415f672e9d0f779c29ac45f3c1b3d9477dc3
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2021-11-03 16:08:45 +0000

    Sync LeetCode submission - Maximum Product Subarray (cpp)

--- problems/maximum_product_subarray/solution.cpp
+++ problems/maximum_product_subarray/solution.cpp
@@ -4,25 +4,11 @@ public:
         int ans=nums[0], maxi=nums[0], mini=nums[0];
         
         for(int i=1;i<nums.size();i++){
-            if(nums[i] < 0) swap(mini, maxi);
-            maxi = max(nums[i], nums[i]*maxi);
-            mini = min(nums[i], nums[i]*mini);
-            
-            ans=max(maxi, ans);
-        }
-        return ans;
-    }
-};
-
-/*
-int ans=nums[0], maxi=nums[0], mini=nums[0];
-        
-        for(int i=1;i<nums.size();i++){
             int currMax = max({nums[i], nums[i]*mini, nums[i]*maxi});
             mini = min({nums[i], nums[i]*mini, nums[i]*maxi});
             maxi = currMax;
             ans=max(maxi, ans);
         }
         return ans;
-*/
-
+    }
+};
