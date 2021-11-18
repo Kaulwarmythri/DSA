aed146ab779484bab6b6429019bd7337d4a00bae
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2021-11-18 12:57:30 +0000

    Sync LeetCode submission - Perfect Squares (cpp)

---
+++ problems/perfect_squares/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        vector<int> dp(n+1, INT_MAX);
+        dp[0]=0;
+        int count=1;
+        while(count*count<=n){
+            for(int i=count*count;i<=n;i++) dp[i]=min(dp[i], dp[i-count*count]+1);
+            
+            count++;
+        }
+        return dp[n];
+    }
+};
