2bbfdccf67ef5fbdce3f9998f90b1a47f95fdcc6
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2021-12-01 02:00:17 +0000

    Sync LeetCode submission - Maximum Product Subarray (cpp)

--- problems/maximum_product_subarray/solution.cpp
+++ problems/maximum_product_subarray/solution.cpp
@@ -12,3 +12,13 @@ public:
         return ans;
     }
 };
+
+//OR just sort the elements are return :
+/*
+int maxProduct(vector<int>& nums) {
+        
+        sort(nums.begin(), nums.end());
+        return max(nums[0]*nums[1]*nums[n-1], nums[n-1]*nums[n-2]*nums[n-3]);
+}
+
+*/
