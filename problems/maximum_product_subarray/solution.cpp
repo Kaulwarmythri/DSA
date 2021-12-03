1378a52e4fbf4d4ce13bf29cffe0939e99c69c82
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2021-12-03 00:39:43 +0000

    Sync LeetCode submission - Maximum Product Subarray (cpp)

--- problems/maximum_product_subarray/solution.cpp
+++ problems/maximum_product_subarray/solution.cpp
@@ -4,21 +4,25 @@ public:
         int ans=nums[0], maxi=nums[0], mini=nums[0];
         
         for(int i=1;i<nums.size();i++){
-            int currMax = max({nums[i], nums[i]*mini, nums[i]*maxi});
-            mini = min({nums[i], nums[i]*mini, nums[i]*maxi});
-            maxi = currMax;
+            if(nums[i] < 0) swap(mini, maxi);
+            maxi = max(nums[i], nums[i]*maxi);
+            mini = min(nums[i], nums[i]*mini);
+            
             ans=max(maxi, ans);
         }
         return ans;
     }
 };
 
-//OR just sort the elements are return :
 /*
-int maxProduct(vector<int>& nums) {
+int ans=nums[0], maxi=nums[0], mini=nums[0];
         
-        sort(nums.begin(), nums.end());
-        return max(nums[0]*nums[1]*nums[n-1], nums[n-1]*nums[n-2]*nums[n-3]);
-}
-
+        for(int i=1;i<nums.size();i++){
+            int currMax = max({nums[i], nums[i]*mini, nums[i]*maxi});
+            mini = min({nums[i], nums[i]*mini, nums[i]*maxi});
+            maxi = currMax;
+            ans=max(maxi, ans);
+        }
+        return ans;
 */
+
