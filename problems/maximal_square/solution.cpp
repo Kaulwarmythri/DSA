1b7660c9a0e91d34a57bc2ca1ccfc922b402c3a6
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2021-12-17 03:57:57 +0000

    Sync LeetCode submission - Maximal Square (cpp)

--- problems/maximal_square/solution.cpp
+++ problems/maximal_square/solution.cpp
@@ -2,14 +2,15 @@ class Solution {
 public:
     int maximalSquare(vector<vector<char>>& matrix) {
         int m = matrix.size(), n = matrix[0].size(), maxi = 0, prev;
-        vector<int> dp(n+1, 0);
-        for(int i = 0;i < m;i++) {
+        vector<int> dp(n, 0);
+        for(int i=m-1;i>=0;i--){
             int prev = 0;
-            for(int j = 1;j <= n;j++) {
-                int tmp = dp[j];
-                if(matrix[i][j-1] == '1') dp[j] = 1 + min({prev, dp[j], dp[j-1]});
-                else dp[j] = 0;
-                prev = tmp;
+            for(int j=n-1;j>=0;j--){
+                int temp = dp[j];
+                if(i==m-1 || j==n-1 || matrix[i][j]=='0') dp[j] = matrix[i][j]-'0';
+                else dp[j] = 1 + min({dp[j], dp[j+1], prev});
+                prev = temp;
+                
                 maxi = max(maxi, dp[j]);
             }
         }
