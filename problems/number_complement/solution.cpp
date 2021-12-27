cc94e790604150a09289a332ce04a60016ddb26d
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2021-12-27 02:06:15 +0000

    Sync LeetCode submission - Number Complement (cpp)

---
+++ problems/number_complement/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findComplement(int num) {
+        int n = num, ans = 1;
+        
+        while(n != 1){
+            n >>= 1;
+            ans <<= 1;
+            ans |= 1;
+        }
+        
+        return ans^num;
+    }
+};
