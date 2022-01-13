cee4906e25ea81e2c4d1225f1c398f7c525501d9
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-01-13 10:18:32 +0000

    Sync LeetCode submission - Cherry Pickup II (cpp)

--- problems/cherry_pickup_ii/solution.cpp
+++ problems/cherry_pickup_ii/solution.cpp
@@ -1,55 +1,32 @@
 class Solution {
-private:
-    vector<vector<int>> indices = {{1,1}, {1,0} ,{1,-1}};
-    int rsize=0;
-    int colsize=0;
-    int dp[100][100][100];
+    vector<vector<vector<int>>> dp;
+    int m, n;
 public:
-    int recur(vector<vector<int>>& grid, int row, int col1, int col2)
-    {
-            
-        if(row<0 or col1<0  or col2<0 or row>=rsize or col1>=colsize  or col2>=colsize)
-           return 0;
+    int cherryPickup(vector<vector<int>>& grid) {
+        m = grid.size(); n = grid[0].size();
+        dp.resize(m, vector<vector<int>>(n, vector<int>(n, -1)));
+        return solve(grid, 0, 0, n-1);
+    }
+    
+    int solve(vector<vector<int>> &grid, int r, int c1, int c2){
+        if(r >= m || c1 >= n || c2 >= n || c1 < 0 || c2 < 0) return 0;
         
-        int sum=0;
+        if(r == m) return grid[r][c1];
         
-        if(dp[row][col1][col2]!=-1)
-            return dp[row][col1][col2];
+        if(dp[r][c1][c2] != -1) return dp[r][c1][c2];
         
-        for(int i=0;i<indices.size();i++)
-        {
-                int a=row,b=col1;
-                a += indices[i][0];
-                b += indices[i][1];
-            
-            for(int j=0;j<indices.size();j++)
-            {
-                int c=row,d=col2;
-                c += indices[j][0];
-                d += indices[j][1];
-                
-                if(a<0 or b<0 or c<0 or d<0 or a>=rsize or b>=colsize or c>=rsize or d>=colsize)
-                  continue;
-                
-                if(a==c and b==d) 
-                  continue;
-                
-
-                sum= max(sum, grid[a][b]+grid[c][d] + recur(grid, a, b, d));
-                
+        int ans = 0, dir[3] = {-1, 0, 1}, total = INT_MIN;
+        
+        ans = grid[r][c1];
+        if(c1 != c2) ans += grid[r][c2];
+        
+        for(int i=0; i<3; i++){
+            for(int j=0; j<3; j++){
+                total = max(total, solve(grid, r+1, c1+dir[i], c2+dir[j]));
             }
         }
+        ans += total;
+        return dp[r][c1][c2] = ans;
         
-        return dp[row][col1][col2]=sum;
-    }
-    int cherryPickup(vector<vector<int>>& grid) 
-    {
-        rsize=grid.size();
-        colsize=grid[0].size();
-        
-        memset(dp,-1,sizeof(dp));
-        
-        return recur( grid,0,0,grid[0].size()-1) +grid[0][0]+grid[0][grid[0].size()-1];
-    
     }
 };
