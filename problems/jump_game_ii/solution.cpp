19509fd9ff61bf20fc01a7eab174b3092d8d5f28
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-01-15 02:13:40 +0000

    Sync LeetCode submission - Jump Game II (cpp)

--- problems/jump_game_ii/solution.cpp
+++ problems/jump_game_ii/solution.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        if(nums.size()==1) return 0;
-        int max_reach=0, curr_reach=0, jumps=0;
+        int max_reach = 0, curr_reach = 0, jumps = 0;
         
-        for(int i=0;i<nums.size()-1;i++){
+        for(int i=0; i<nums.size()-1; i++){
             max_reach = max(max_reach, nums[i]+i);
-            if(i==curr_reach) curr_reach = max_reach, jumps++;   
-            if(curr_reach >= nums.size()) break;
+            if(i==curr_reach){
+                curr_reach = max_reach;
+                jumps++;
+            }
         }
         return jumps;
+        
     }
 };
