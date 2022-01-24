ed38c6e1f9ffea2a9d738c14351d155e6fd544fd
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-01-24 02:19:34 +0000

    Sync LeetCode submission - Check If String Is a Prefix of Array (cpp)

---
+++ problems/check_if_string_is_a_prefix_of_array/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPrefixString(string s, vector<string>& words) {
+        string temp = "";
+        
+        for(int i=0; i<words.size(); i++){
+            temp += words[i];
+            if(temp.compare(s)==0) return true;
+        }
+        return false;
+    }
+};
