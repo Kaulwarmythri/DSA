37c396332c378f6a4a337f840071938b02b79a0a
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-01-29 13:45:45 +0000

    Sync LeetCode submission - Largest Rectangle in Histogram (cpp)

--- problems/largest_rectangle_in_histogram/solution.cpp
+++ problems/largest_rectangle_in_histogram/solution.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    int largestRectangleArea(vector<int>& heights) {
-        int ans=INT_MIN, area_with_top=0, top=0, i=0, n=heights.size();
+    int largestRectangleArea(vector<int>& h) {
         stack<int> s;
+        int ans = INT_MIN, area_with_top = 0, top = 0, i = 0;
         
-        while(i<n){
-            if(s.empty() || heights[s.top()]<=heights[i]) s.push(i++);
+        while(i < h.size()){
+            if(s.empty() || h[s.top()] <= h[i]) s.push(i++);
             else{
                 top = s.top(); s.pop();
-                area_with_top = heights[top]*(s.empty() ? i : i-s.top()-1);
+                area_with_top = h[top]*(s.empty() ? i : (i-s.top()-1));
                 ans = max(ans, area_with_top);
             }
         }
         while(!s.empty()){
             top = s.top(); s.pop();
-            area_with_top = heights[top]*(s.empty() ? i : i-s.top()-1);
+            area_with_top = h[top]*(s.empty() ? i : i-s.top()-1);
             ans = max(ans, area_with_top);
         }
-         
         return ans;
     }
 };
