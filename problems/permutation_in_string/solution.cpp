24129b3323e585a24ed1fb21a2c72145cb565c03
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-02-02 02:13:01 +0000

    Sync LeetCode submission - Permutation in String (cpp)

--- problems/permutation_in_string/solution.cpp
+++ problems/permutation_in_string/solution.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        int i = 0, m = s1.size(), n = s2.size();
-        if(m > n) return false;
-        vector<int> cnt1(26, 0), cnt2(26, 0);
-        
-        for(i=0; i<m; i++){
-            cnt1[s1[i]-'a']++;
-            cnt2[s2[i]-'a']++;
+        int n=s1.size(), m=s2.size(), i;
+        if(n > m) return false;
+        vector<int> c1(26,0),c2(26,0);
+        for(i=0; i<n; i++){
+            c1[s1[i]-'a']++;
+            c2[s2[i]-'a']++;
         }
-        
-        for(; i<n; i++){
-            if(cnt1 == cnt2) return true;
-            cnt2[s2[i-m]-'a']--;
-            cnt2[s2[i]-'a']++;
+        for(i; i<m; i++){
+            if(c1==c2) return true;
+            c2[s2[i-n]-'a']--;
+            c2[s2[i]-'a']++;
         }
-        return cnt1 == cnt2 ? true : false;
+        return c1 == c2 ? true : false;
     }
 };
