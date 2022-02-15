1ce3c70ea3b335274cbc3b3d80cd061f546cfd1b
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-02-15 02:37:15 +0000

    Sync LeetCode submission - Single Number (cpp)

--- problems/single_number/solution.cpp
+++ problems/single_number/solution.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int x_or=0;
-        for(int i=0;i<nums.size();i++) x_or^=nums[i];
-        
-        return x_or;
+        int xOR = 0;
+        for(auto num : nums) xOR ^= num;
+        return xOR;
     }
 };
