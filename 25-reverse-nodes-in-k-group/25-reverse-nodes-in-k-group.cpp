17f2e140bc54cc0b015ba8ed1b8ef5b6be74232a
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-02-16 12:21:18 +0530

    Time: 12 ms (94.68%), Space: 11.6 MB (31.70%) - LeetHub

--- 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.cpp
+++ 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.cpp
@@ -1,13 +1,11 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+/** * Definition for singly-linked list.
+    *     struct ListNode {
+    *     int val;
+    *     ListNode *next;
+    *     ListNode() : val(0), next(nullptr) {}
+    *     ListNode(int x) : val(x), next(nullptr) {}
+    *     ListNode(int x, ListNode *next) : val(x), next(next) {} * };*/
+
 class Solution {
 public:
     ListNode* reverseKGroup(ListNode* head, int k) {
@@ -20,15 +18,15 @@ public:
     ListNode *reverseGroups(ListNode *head, int k, int n){
         int cnt = 0;
         ListNode *curr = head, *prev = NULL, *nxt = NULL;
-        while(curr && cnt < k && n-k >= 0){
+        while(curr && cnt < k){
             nxt = curr->next;
             curr->next = prev;
             prev = curr;
             curr = nxt;
             cnt++;
         }
-        n = n - k;
-        if(curr && n-k >= 0) head->next = reverseGroups(nxt, k, n);
+        n -= k;
+        if(curr && n-k >= 0) head->next = reverseGroups(curr, k, n);
         else head->next = nxt;
         
         return prev;
