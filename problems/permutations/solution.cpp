ba84a4a806f42b37b0e80e2c9fe18439601e9ba8
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-02-17 15:53:25 +0000

    Sync LeetCode submission - Permutations (cpp)

---
+++ problems/permutations/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int n;
+    vector<vector<int>> permute(vector<int>& nums) {
+        n = nums.size();
+        vector<vector<int>> ans;
+        solve(nums, 0, ans);
+        return ans;
+    }
+    
+    void solve(vector<int> &nums, int i, vector<vector<int>> &ans){
+        if(i == n){
+            ans.push_back(nums);
+            return;
+        }
+        
+        for(int j=i; j<n; j++){
+            swap(nums[i], nums[j]);
+            solve(nums, i+1, ans);
+            swap(nums[i], nums[j]);
+        }
+    }
+};
