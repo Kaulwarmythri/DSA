2a26ef8e865817e400b5a045061e6182fbeac103
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-02-17 11:42:27 +0000

    Sync LeetCode submission - Combination Sum III (cpp)

---
+++ problems/combination_sum_iii/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<vector<int>> ans;
+        vector<int> curr;
+        solve(1, k, n, curr, ans);
+        return ans;
+    }
+    
+    void solve(int i, int k, int n, vector<int> &curr, vector<vector<int>> &ans){
+        if(k==0){
+            if(n==0)ans.push_back(curr);
+            return;
+        }
+        for(int j=i; j<=9; j++){
+            if(n - j >= 0){
+                curr.push_back(j);
+                solve(j+1, k-1, n-j, curr, ans);
+                curr.pop_back();
+            }
+        }
+    }
+};
