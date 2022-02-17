d86064f8c0ebf5358efd2620eea205db982d52f3
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-02-17 14:00:49 +0000

    Sync LeetCode submission - Combination Sum IV (cpp)

--- problems/combination_sum_iv/solution.cpp
+++ problems/combination_sum_iv/solution.cpp
@@ -1,13 +1,35 @@
 class Solution {
 public:
-    int combinationSum4(vector<int>& nums, int target) {
-        vector<uint> dp(target+1, 0);
-        dp[0]=1;
-        for(int currentTarget=1;currentTarget<=target;currentTarget++){
-            for(auto num : nums){
-                if(num<=currentTarget) dp[currentTarget]+=dp[currentTarget-num];
-            }
+    vector<int> dp;
+    int combinationSum4(vector<int>& nums, int X) {
+        dp.resize(X+1, -1);
+        dp[0] = 1;
+        solve(nums, X);
+        return dp[X];
+    }
+    int solve(vector<int> nums, int X){
+        if(dp[X] != -1) return dp[X];
+        int ans = 0;
+        for(auto num : nums){
+            if(num <= X) ans += solve(nums, X-num);
         }
-        return dp[target];
+        return dp[X] = ans;
+    }
+    
+};
+
+
+/* Recursive Solution for how to approach the above dp solution
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int X) {
+        if(X == 0) return 1;
+        int ans = 0;
+        for(auto num : nums){
+            if(num <= X) ans += combinationSum4(nums, X-num);
+        }
+        return ans;
     }
 };
+*/
+
