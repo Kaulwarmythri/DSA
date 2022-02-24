58f16afa6da25d1b12b939022cb35a101dfe8670
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-02-24 02:11:45 +0000

    Sync LeetCode submission - Sort List (cpp)

--- problems/sort_list/solution.cpp
+++ problems/sort_list/solution.cpp
@@ -1,51 +1,50 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+/** * Definition for singly-linked list.
+    * struct ListNode {
+    *     int val;
+    *     ListNode *next;
+    *     ListNode() : val(0), next(nullptr) {}
+    *     ListNode(int x) : val(x), next(nullptr) {}
+    *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+    * };*/
+
 class Solution {
 public:
     ListNode* sortList(ListNode* head) {
         if(!head || !head->next) return head;
-        ListNode *slow=head, *fast=head;
         
+        ListNode *fast = head, *slow = head;
         while(fast->next && fast->next->next){
-            slow = slow->next;
             fast = fast->next->next;
+            slow = slow->next;
         }
-        ListNode* head2 = slow->next;
+        ListNode *head2 = slow->next;
         slow->next = NULL;
-        
         ListNode *A = sortList(head), *B = sortList(head2);
-        ListNode *final_head = merge(A, B);   
+        ListNode *finalhead = merge(A, B);
         
-        return final_head;
+        return finalhead;
     }
+    
     ListNode *merge(ListNode *A, ListNode *B){
-        ListNode *head;
+        ListNode *head, *curr;
         if(A->val <= B->val){
             head = A;
-            A = A->next;   
-        }else{
+            A = A->next;
+        }else {
             head = B;
             B = B->next;
         }
-        ListNode *curr = head;
+        
+        curr = head;
         while(A && B){
             if(A->val <= B->val){
                 curr->next = A;
-                curr = A;
                 A = A->next;
-            }else{
+            }else {
                 curr->next = B;
-                curr = B;
                 B = B->next;
             }
+            curr = curr->next;
         }
         if(A) curr->next = A;
         if(B) curr->next = B;
