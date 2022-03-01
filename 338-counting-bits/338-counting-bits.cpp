c50b3d5097fbd6cd2cfde0ca1c01d5903d495640
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-03-01 07:38:59 +0530

    Time: 16 ms (10.60%), Space: 7.7 MB (89.06%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,10 +1,30 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
+        vector<int> ans(n+1, 0);
+        int p = 1, pow = 1;
+        for(int i=1;i<n+1;i++){
+            if(i==pow){
+                ans[i] = 1;
+                pow <<= 1;
+                p = 1;
+            }else {
+                ans[i] = ans[p] + 1;
+                p++;
+            }
+        }
+        return ans;
+    }
+};
+
+
+/*class Solution {
+public:
+    vector<int> countBits(int n) {
         vector<int> ans(n+1);
         for(int i=0; i<=n; i++){
             ans[i] = __builtin_popcount(i);
         }
         return ans;
     }
-};
+};*/
