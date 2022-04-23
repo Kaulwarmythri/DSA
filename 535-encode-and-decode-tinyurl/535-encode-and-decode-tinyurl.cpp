e2f0379250a9342779d5e0be0eef2c3c967a3c4c
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-04-23 08:47:39 +0530

    Time: 12 ms (10.54%), Space: 6.8 MB (66.28%) - LeetHub

--- 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
@@ -5,9 +5,9 @@ public:
 
     // Encodes a URL to a shortened URL.
     string encode(string longUrl) {
-        s += (char) (rand()/RAND_MAX);
+        s += (char) (rand()/RAND_MAX)*100;
         while(m.find(s) != m.end()) 
-            s += (char) (rand()/RAND_MAX);
+            s += (char) (rand()/RAND_MAX)*100;
         
         m.insert({s, longUrl});
         
