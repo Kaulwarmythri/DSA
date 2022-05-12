35279c20da629b99bd69555bc96eb050869f4fb6
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-05-12 16:27:25 +0530

    Time: 63 ms (24.57%), Space: 9.2 MB (56.98%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -4,17 +4,19 @@ public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
         n = nums.size();
         set<vector<int>> ans;
+        
         solve(nums, 0, ans);
+        
         return vector<vector<int>>(ans.begin(), ans.end());
     }
     
-    void solve(vector<int> &nums, int i, set<vector<int>> &ans){
-        if(i==nums.size()){
+    void solve(vector<int> &nums, int i, set<vector<int>> &ans) {
+        if(i == n) {
             ans.insert(nums);
             return;
         }
         
-        for(int j=i; j<n; j++){
+        for(int j=i; j<n; j++) {
             swap(nums[i], nums[j]);
             solve(nums, i+1, ans);
             swap(nums[i], nums[j]);
