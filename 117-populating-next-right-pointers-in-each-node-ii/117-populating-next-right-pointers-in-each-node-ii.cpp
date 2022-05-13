440326aad726fbdc393e99fe3d87648bc9ce0dae
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-05-14 02:57:11 +0530

    Time: 24 ms (25.47%), Space: 17.4 MB (81.13%) - LeetHub

---
+++ 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.cpp
@@ -0,0 +1,66 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if(!root) return NULL;
+        
+        if(root->left) root->left->next = root->right ? root->right : findNext(root->next);
+        
+        if(root->right) root->right->next = findNext(root->next);
+        
+        connect(root->right);
+        
+        connect(root->left);
+        
+        return root;
+    }
+    
+    Node *findNext(Node *root) {
+        if(!root) return NULL;
+        if(root->left) return root->left;
+        
+        if(root->right) return root->right;
+        
+        return findNext(root->next);
+    }
+};
+
+
+
+//  Node* connect(Node* root) {
+//         if(!root) return NULL;
+        
+//         if(root->left) root->left->next = root->right ? root->right : find(root->next);
+        
+//         if(root->right) root->right->next = find(root->next);
+        
+//         connect(root->right);
+//         connect(root->left);
+        
+//         return root;
+//     }
+    
+//     Node *find(Node *root){
+//         if(!root) return NULL;
+//         if(root->left) return root->left;
+//         if(root->right) return root->right;
+        
+//         return find(root->next);
+//     }
