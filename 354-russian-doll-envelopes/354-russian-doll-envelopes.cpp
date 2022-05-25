b8d2aa19be6ff7a6a4c8dc156ba6050cf4b9b02e
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-05-25 09:03:56 +0530

    Time: 680 ms (16.67%), Space: 87.8 MB (17.23%) - LeetHub

--- 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
+++ 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
@@ -1,18 +1,19 @@
+bool compare(vector<int> &a, vector<int> &b) {
+    return a[0] < b[0] || (a[0] == b[0] && a[1] > b[1]);
+}
 class Solution {
 public:
     int maxEnvelopes(vector<vector<int>>& E) {
-        sort(E.begin(), E.end(), [](vector<int>& a, vector<int>& b) 
-             -> bool {return a[0] == b[0] ? b[1] < a[1] : a[0] < b[0];});
-        vector<int> dp;
-        for (auto& env : E) {
-            
-            auto it = lower_bound(dp.begin(), dp.end(), env[1]);
-            
-            if (it == dp.end()) dp.push_back(env[1]);
-            
-            else *it = env[1];
-        }
+        sort(E.begin(), E.end(), compare);
         
+        vector<int> dp;
+        
+        for(auto e : E) {
+            auto it = lower_bound(dp.begin(), dp.end(), e[1]);
+            if(it == dp.end()) dp.push_back(e[1]);
+            
+            else *it = e[1];
+        }
         return dp.size();
     }
 };
