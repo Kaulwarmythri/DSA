f5fcc0f7b496bc8269e286422331be975b800778
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-06-05 07:24:30 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (79.40%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.cpp
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -3,24 +3,31 @@ class Solution {
 public:
     int totalNQueens(int n) {
         s = n;
-        vector<bool> cols(n), diags(2*n-1, false), anti_diags(2*n-1, false);
+        vector<bool> cols(n, false), diags(n, false), anti_diags(n, false);
         
         return solve(0, cols, diags, anti_diags);
+        
     }
     
-    int solve(int row, vector<bool> &cols, vector<bool> &diags, vector<bool> &a_diags) {
+    int solve(int row, vector<bool> &cols, vector<bool> &diags, vector<bool> &anti_diags) {
+        if(row >= s) {
+            return 1;
+        }
+        
         int count = 0;
-        if(row == s) return 1;
         
         for(int j=0; j<s; j++) {
-            if(!cols[j] && !diags[row+j] && !a_diags[row-j+s-1]) {
-                cols[j] = diags[row+j] = a_diags[row-j+s-1] = true;
+            if(!cols[j] && !diags[row+j] && !anti_diags[row-j+s-1]) {
                 
-                count += solve(row+1, cols, diags, a_diags);
+                cols[j] = diags[row+j] = anti_diags[row-j+s-1] = true;
                 
-                cols[j] = diags[row+j] = a_diags[row-j+s-1] = false;
+                count += solve(row+1, cols, diags, anti_diags);
+                
+                cols[j] = diags[row+j] = anti_diags[row-j+s-1] = false;     
             }
         }
         return count;
     }
+    
+    
 };
