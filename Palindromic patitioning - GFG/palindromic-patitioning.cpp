c03d72e932e04de367f9b6264cde8fca4c32f9e9
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-06-08 18:54:15 +0530

    Added solution - LeetHub

---
+++ Palindromic patitioning - GFG/palindromic-patitioning.cpp
@@ -0,0 +1,56 @@
+// { Driver Code Starts
+// Initial Template for c++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int palindromicPartition(string s) {
+        int n = s.size();
+        vector<vector<bool>> dp(n, vector<bool>(n, 0));
+        
+        vector<vector<int>> cp(n, vector<int>(n, 0));
+        
+        for(int i=0; i<n; i++) {
+            dp[i][i] = 1,
+            cp[i][i] = 0;
+        }
+        
+        for(int gap=1; gap<n; gap++) {
+            for(int i=0, j=gap; j < n; i++, j++) {
+                if(j-i == 1) dp[i][j] = (s[i] == s[j]);
+                else {
+                    dp[i][j] = (s[i]==s[j]) && dp[i+1][j-1];
+                }
+                if(dp[i][j]) cp[i][j] = 0;
+                
+                else {
+                    cp[i][j] = INT_MAX;
+                
+                    for(int k=i; k<j; k++) cp[i][j] = min(cp[i][j], cp[i][k] + 1 + cp[k+1][j]);
+                }
+            }
+        }
+        return cp[0][n-1];
+    }
+};
+
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string str;
+        cin>>str;
+        
+        Solution ob;
+        cout<<ob.palindromicPartition(str)<<"\n";
+    }
+    return 0;
+}  // } Driver Code Ends
