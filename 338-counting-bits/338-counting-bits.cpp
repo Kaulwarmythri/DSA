7a44956c493972db68751de532abc7163485281f
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-06-09 12:21:00 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (88.31%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -3,9 +3,10 @@ public:
     vector<int> countBits(int n) {
         vector<int> ans(n+1, 0);
         
-        for(int i=1; i<=n; i++){
-            ans[i] = (i&1 ? 1 : 0) + ans[i/2]; 
+        for(int i=1; i<=n; i++) {
+            ans[i] = ans[i/2] + ((i&1) ? 1 : 0);
         }
+        
         return ans;
     }
 };
