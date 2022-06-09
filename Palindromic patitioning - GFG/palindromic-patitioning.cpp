de06a93d1911ccfab70d9971104740a6b144b7a6
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-06-09 06:46:46 +0530

    Added solution - LeetHub

--- Palindromic patitioning - GFG/palindromic-patitioning.cpp
+++ Palindromic patitioning - GFG/palindromic-patitioning.cpp
@@ -11,26 +11,23 @@ class Solution{
 public:
     int palindromicPartition(string s) {
         int n = s.size();
-        vector<vector<bool>> dp(n, vector<bool>(n, 0));
-        
-        vector<vector<int>> cp(n, vector<int>(n, 0));
+        vector<vector<bool>> dp(n, vector<bool>(n));
+        vector<vector<int>> cp(n, vector<int>(n));
         
         for(int i=0; i<n; i++) {
-            dp[i][i] = 1,
+            dp[i][i] = true;
             cp[i][i] = 0;
         }
         
         for(int gap=1; gap<n; gap++) {
-            for(int i=0, j=gap; j < n; i++, j++) {
+            for(int i=0, j=gap; j<n; i++, j++) {
                 if(j-i == 1) dp[i][j] = (s[i] == s[j]);
-                else {
-                    dp[i][j] = (s[i]==s[j]) && dp[i+1][j-1];
-                }
-                if(dp[i][j]) cp[i][j] = 0;
+                else dp[i][j] = (s[i] == s[j]) && dp[i+1][j-1];
                 
+                if(dp[i][j]) cp[i][j] = 0;
                 else {
                     cp[i][j] = INT_MAX;
-                
+                    
                     for(int k=i; k<j; k++) cp[i][j] = min(cp[i][j], cp[i][k] + 1 + cp[k+1][j]);
                 }
             }
@@ -38,7 +35,6 @@ public:
         return cp[0][n-1];
     }
 };
-
 
 // { Driver Code Starts.
 
