beb0f27d4e16e6fefaf00de345fc90fe81e5f609
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-06-10 08:11:10 +0530

    Time: 15 ms (89.77%), Space: 16.6 MB (52.30%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -4,7 +4,8 @@ public:
         int n = nums.size(), max_reach = 0, curr_reach = 0, jumps = 0;
         
         for(int i=0; i<n-1; i++) {
-            max_reach = max(max_reach, nums[i]+i);
+            max_reach = max(max_reach, nums[i] + i);
+            
             if(i == curr_reach) {
                 curr_reach = max_reach;
                 jumps++;
