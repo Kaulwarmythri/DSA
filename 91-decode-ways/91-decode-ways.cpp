a859298c22a081ed145d50af1ab9f2b084f10df3
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-06-11 16:59:13 +0530

    Time: 3 ms (56.87%), Space: 7.6 MB (13.35%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,22 +1,20 @@
 class Solution {
     int n;
     vector<int> dp;
-    
 public:
     int numDecodings(string s) {
         n = s.size();
         dp.resize(n+1, -1);
         dp[n] = 1;
-        return s.empty() ? 0 : solve(s, 0, dp);
+        return solve(s, 0);
     }
     
-    int solve(string s, int i, vector<int> &dp) {
-        if(s[i] == '0') dp[i] = 0;
+    int solve(string s, int i) {
+        if(s[i] == '0') return dp[i] = 0;
         if(dp[i] > -1) return dp[i];
         
-        int res = solve(s, i+1, dp);
-        if(i < n-1 && (s[i] == '1' || (s[i] == '2' && s[i+1] < '7')))
-            res += solve(s, i+2, dp);
+        int res = solve(s, i+1);
+        if(i < n-1 && (s[i] == '1' || (s[i] == '2' && s[i+1] < '7'))) res+= solve(s, i+2);
         
         return dp[i] = res;
     }
