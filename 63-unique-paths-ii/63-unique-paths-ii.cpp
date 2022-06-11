f112aed69ab11d18a1947bacfbc983c2aba607ed
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-06-11 09:49:43 +0530

    Time: 6 ms (40.82%), Space: 7.4 MB (98.38%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -1,26 +1,19 @@
 class Solution {
-    vector<vector<int>> dp;
-    int m, n;
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& og) {
-        m = og.size(), n = og[0].size();
-        dp.resize(m, vector<int>(n));
+        int m = og.size(), n = og[0].size();
         
-        return solve(og, 0, 0);
-    }
-    
-    int solve(vector<vector<int>> &og, int i, int j) {
-        if(i < 0 || j < 0 || i >= m || j >= n) return 0;
+        og[0][0] ^= 1;
+        for(int i=1; i<m; i++) og[i][0] = !og[i][0] & og[i-1][0];
         
-        if(og[i][j]) return dp[i][j] = 0;
+        for(int j=1; j<n; j++) og[0][j] = !og[0][j] & og[0][j-1];
         
-        if(i == m-1 && j == n-1) return 1;
+        for(int i=1; i<m; i++) {
+            for(int j=1; j<n; j++) {
+                og[i][j] = (og[i][j]) ? 0 : og[i-1][j] + og[i][j-1];
+            }
+        }
         
-        if(dp[i][j]) return dp[i][j];
-        
-        return dp[i][j] = solve(og, i+1, j) + solve(og, i, j+1);
+        return og[m-1][n-1];
     }
 };
-
-
-
