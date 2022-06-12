a6f4a5d4fb0cb642977d58e204fa68072e729309
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-06-12 10:59:26 +0530

    Time: 493 ms (100.00%), Space: 80.4 MB (100.00%) - LeetHub

--- 2304-minimum-path-cost-in-a-grid/2304-minimum-path-cost-in-a-grid.cpp
+++ 2304-minimum-path-cost-in-a-grid/2304-minimum-path-cost-in-a-grid.cpp
@@ -10,11 +10,11 @@ public:
         for(int i=1; i<m; i++) {
             for(int j=0; j<n; j++) {
                 for(int k=0; k<n; k++) {
-                    dp[i][k] = min(dp[i][k], grid[i][k] + dp[i-1][j] + mc[grid[i-1][j]][k]);
+                    dp[i][k] = 
+                        min(dp[i][k], grid[i][k] + mc[grid[i-1][j]][k] + dp[i-1][j]);
                 }
             }
         }
-        
         return *min_element(dp[m-1].begin(), dp[m-1].end());
     }
 };
