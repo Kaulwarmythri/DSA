4d079d7a8cc229ab3565409d63580f5120d18869
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-06-12 10:48:45 +0530

    Time: 514 ms (100.00%), Space: 80.4 MB (100.00%) - LeetHub

---
+++ 2304-minimum-path-cost-in-a-grid/2304-minimum-path-cost-in-a-grid.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minPathCost(vector<vector<int>>& grid, vector<vector<int>>& mc) {
+        int m = grid.size(), n = grid[0].size();
+        
+        vector<vector<int>> dp(m, vector<int>(n, INT_MAX));
+        
+        dp[0] = grid[0];
+        
+        for(int i=1; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                for(int k=0; k<n; k++) {
+                    dp[i][k] = min(dp[i][k], grid[i][k] + dp[i-1][j] + mc[grid[i-1][j]][k]);
+                }
+            }
+        }
+        
+        return *min_element(dp[m-1].begin(), dp[m-1].end());
+    }
+};
