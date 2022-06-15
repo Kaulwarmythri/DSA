ee3005907306d9cefd10178eee025b4d06af771c
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-06-15 10:06:28 +0530

    Time: 4 ms (42.14%), Space: 7.6 MB (13.38%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -11,10 +11,10 @@ public:
     
     int solve(string s, int i) {
         if(s[i] == '0') return dp[i] = 0;
-        if(dp[i] > -1) return dp[i];
+        if(dp[i] != -1) return dp[i];
         
         int res = solve(s, i+1);
-        if(i < n-1 && (s[i] == '1' || (s[i] == '2' && s[i+1] < '7'))) res+= solve(s, i+2);
+        if(i < n-1 && (s[i] == '1' ||  (s[i] == '2' && s[i+1] < '7'))) res += solve(s, i+2);
         
         return dp[i] = res;
     }
