e031d4377c2641823f32ddb30252384267da34ba
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-06-15 07:40:04 +0530

    Time: 144 ms (49.22%), Space: 23.2 MB (26.79%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,36 +1,34 @@
 class Solution {
 public:
-    static bool compare(const string &s1, const string &s2) {
-        return s1.length() < s2.length();
-    }
-    
     int longestStrChain(vector<string>& words) {
         int n = words.size();
-        vector<int> dp(n,1);
-        sort(words.begin(),words.end(),compare);
-        for(int i = 1;i<n;i++)
-        {
-            for(int j=0;j<i;j++)
-            {
-                if(words[i].length()-words[j].length()==1 && lcs(words[i],words[j])==words[j].length() && dp[i]<dp[j]+1)
-                    dp[i] = dp[j] + 1;
-            }
-        }
-        return *max_element(dp.begin(),dp.end());
-    }
-    
-    int lcs(string a, string b) {
-        int m = a.size(), n = b.size();
-        vector<vector<int>> dp(m+1, vector<int>(n+1));
         
-        for(int i=0; i<=m; i++) {
-            for(int j=0; j<=n; j++) {
-                if(i == 0 || j == 0) dp[i][j] = 0;
-                else {
-                    dp[i][j] = (a[i-1] == b[j-1]) ? dp[i-1][j-1] + 1 : max(dp[i][j-1], dp[i-1][j]);
+        sort(words.begin(), words.end(), [](string a, string b)-> bool {
+            return a.size() < b.size();
+        });
+        
+        vector<int> dp(n, 1);
+        
+        for(int i=1; i<n; i++) {
+            for(int j=0; j<i; j++) {
+                if(words[j].size()+1 == words[i].size() && check(words[j], words[i]) && dp[i] < dp[j] + 1) {
+                    dp[i] = dp[j] + 1;
                 }
             }
         }
-        return dp[m][n];
+        return *max_element(dp.begin(), dp.end());
+    }
+    
+    bool check(string a, string b) {
+        int m = a.size(), n = b.size(), missing = 0, i=0, j=0;
+        
+        while(i < m && j < n) {
+            if(a[i] == b[j]) i++, j++;
+            else {
+                if(!missing) missing = 1, j++;
+                else return false;
+            }
+        }
+        return true;
     }
 };
