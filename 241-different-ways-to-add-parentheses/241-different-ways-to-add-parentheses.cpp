e8c7089e21d8a3db0a3fb126be9f5204796c76e2
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-06-20 17:40:01 +0530

    Time: 14 ms (10.19%), Space: 11.7 MB (14.24%) - LeetHub

--- 241-different-ways-to-add-parentheses/241-different-ways-to-add-parentheses.cpp
+++ 241-different-ways-to-add-parentheses/241-different-ways-to-add-parentheses.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
     vector<int> diffWaysToCompute(string s) {
+        vector<int> ans;
         int n = s.size();
-        vector<int> res;
         
         for(int i=0; i<n; i++) {
-            if(s[i] == '+' || s[i] == '-' || s[i] == '*') {
+            if(s[i] == '*' || s[i] == '-' || s[i] == '+') {
                 vector<int> left = diffWaysToCompute(s.substr(0, i));
                 vector<int> right = diffWaysToCompute(s.substr(i+1));
                 
                 for(auto l : left) {
-                    for(auto r : right) {
-                        if(s[i] == '+') res.push_back(l + r);
-                        
-                        else if(s[i] == '-') res.push_back(l - r);
-                        
-                        else res.push_back(l * r);
+                    for(auto r: right) {
+                        if(s[i] == '*') {
+                            ans.push_back(l*r);
+                        } else if(s[i] == '+') {
+                            ans.push_back(l+r);
+                        } else {
+                            ans.push_back(l-r);
+                        }
                     }
                 }
             }
         }
-        if(res.empty()) res.push_back(stoi(s));
-        
-        return res;
+        if(!ans.size()) ans.push_back(stoi(s));
+        return ans;
     }
 };
