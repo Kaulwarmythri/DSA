3e24fd94377e817a7f754fffe3ef2b0b61226ade
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-06-20 14:14:13 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (75.46%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -4,13 +4,11 @@ public:
         int n = nums.size();
         if(n == 1) return nums[0];
         
-        vector<int> dp(n+1, 0);
-        dp[1] = nums[0];
-        dp[2] = nums[1];
+        nums[1] = max(nums[0], nums[1]);
         
-        for(int i=3; i<=n; i++) {
-            dp[i] = nums[i-1] + max(dp[i-2], dp[i-3]);
+        for(int i=2; i<n; i++) {
+            nums[i] = max(nums[i-1], nums[i]+nums[i-2]);
         }
-        return max(dp[n], dp[n-1]);
+        return nums[n-1];
     }
 };
