76980d2809fb8a56591c53c33ca557fdfb5b223f
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-06-21 07:53:00 +0530

    Time: 114 ms (91.67%), Space: 53.7 MB (77.22%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
-    int furthestBuilding(vector<int>& H, int bricks, int ladders) {
-        int i = 0, n = size(H), jumpHeight;
-	
-	    priority_queue<int, vector<int> ,greater<int> > pq;
+    int furthestBuilding(vector<int>& H, int B, int L) {
+        int n = H.size(), i = 0, diff;
         
-        while (i < n - 1 && size(pq) < ladders) {
-            jumpHeight = H[i + 1] - H[i];
-            if(jumpHeight > 0) 
-                pq.push(jumpHeight);
-            i++;            
-	    }
-	
-	    while (i < n - 1) {
-            jumpHeight = H[i + 1] - H[i];
-            
-            if(jumpHeight > 0) {
-			
-                if(!pq.empty() && pq.top() < jumpHeight) {                    
-                    
-                    bricks -= pq.top(); pq.pop(); 
-                    pq.push(jumpHeight);                    
-                }
-                else bricks -= jumpHeight;    
-		    }
-            if(bricks < 0) return i;
+        priority_queue<int, vector<int>, greater<int>> pq;
+        
+        while(i < n-1 && pq.size() < L) {
+            diff = H[i+1] - H[i];
+            if(diff > 0) {
+                pq.push(diff);
+            }
             i++;
         }
-	    return i;
         
+        while(i < n-1) {
+            diff = H[i+1] - H[i];
+            if(diff > 0) {
+                if(pq.size() && pq.top() < diff) {
+                    B -= pq.top(); pq.pop();
+                    pq.push(diff);
+                }
+                else {
+                    B -= diff;
+                }
+            }
+            if(B < 0) return i;
+            i++;
+        }
+        
+        return i;
     }
 };
-
