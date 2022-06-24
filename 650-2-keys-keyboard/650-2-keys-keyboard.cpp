b472da6b4a76f33c2ef0f4d72a8a596a9d45cc28
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-06-24 11:59:33 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (98.41%) - LeetHub

--- 650-2-keys-keyboard/650-2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/650-2-keys-keyboard.cpp
@@ -1,17 +1,31 @@
 class Solution {
 public:
     int minSteps(int n) {
-        vector<int> dp(n+1, 0);
+        int ans = 0, factor=2;
         
-        for(int i=2; i<=n; i++) {
-            dp[i] = i;
-            for(int j=2; j<=i/2; j++) {
-                int x = i - j;
-                if(x%j == 0) {
-                    dp[i] = min(dp[i], dp[j] + 1 + x/j);
-                }
+        while(n > 1) {
+            while(n%factor == 0) {
+                n /= factor;
+                ans += factor;
             }
+            factor++;
         }
-        return dp[n];
+        return ans;
     }
 };
+
+
+// int minSteps(int n) {
+//     vector<int> dp(n+1, 0);
+
+//     for(int i=2; i<=n; i++) {
+//         dp[i] = i;
+//         for(int j=2; j<=i/2; j++) {
+//             int x = i - j;
+//             if(x%j == 0) {
+//                 dp[i] = min(dp[i], dp[j] + 1 + x/j);
+//             }
+//         }
+//     }
+//     return dp[n];
+// }
