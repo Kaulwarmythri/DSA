e0d4491395ef9e6f14ad5989d8b25f0b7fc25b2d
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-06-25 07:54:28 +0530

    Time: 867 ms (36.34%), Space: 15.9 MB (35.41%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
@@ -3,30 +3,24 @@ class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& grid) {
         m = grid.size(), n = grid[0].size();
-        if(m == 1) return grid[0][0];
         
         vector<vector<int>> dp(m, vector<int>(n, -1));
         
-        int ans = INT_MAX;
+        dp[0] = grid[0];
         
-        for(int j=0; j<n; j++) {
-            ans = min(ans, path(grid, 0, j, dp));
+        for(int i=1; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                int ans = INT_MAX;
+                
+                for(int k=0; k<n; k++) {
+                    if(k == j) continue;
+                    ans = min(ans, grid[i][j] + dp[i-1][k]);
+                }
+                dp[i][j] = ans;
+            }
         }
         
-        return ans;
-    }
-    
-    int path(vector<vector<int>>& grid, int i, int j, vector<vector<int>> &dp) {
-        if(i == m) return 0;
-        
-        if(dp[i][j] != -1) return dp[i][j];
-        
-        int ans = INT_MAX;
-        
-        for(int col=0; col<n; col++) {
-            if(col == j) continue;
-            ans = min(ans, path(grid, i+1, col, dp) + grid[i][j]);
-        }
-        return dp[i][j] = ans;
+        return *min_element(dp[n-1].begin(), dp[n-1].end());
     }
 };
+
