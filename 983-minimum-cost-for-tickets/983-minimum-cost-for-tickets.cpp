82189f34f9d40e905a11ac0bc5870214d7bcdbf1
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-06-25 12:50:52 +0530

    Time: 3 ms (87.55%), Space: 9.6 MB (70.66%) - LeetHub

--- 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.cpp
+++ 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.cpp
@@ -1,32 +1,21 @@
 class Solution {
-    int n;
 public:
     int mincostTickets(vector<int>& days, vector<int>& costs) {
-        n = days.size();
-        
-        vector<int> dp(n+1, -1);
-        dp[n] = 0;
-        
-        return solve(days, costs, 0, dp);
-    }
-    
-    int solve(vector<int>& days, vector<int>& costs, int idx, vector<int> &dp) {
-        if(idx >= n) return 0;
-        
-        if(dp[idx] != -1) return dp[idx];
-        int i;
-        int day_1 = costs[0] + solve(days, costs, idx+1, dp);
-        
-        for(i=idx; i<n && days[i] < days[idx]+7; i++);
-        
-        int day_7 = costs[1] + solve(days, costs, i, dp);
-        
-        for(i=idx; i<n && days[i] < days[idx]+30; i++);
-            
-        int day_30 = costs[2] + solve(days, costs, i, dp);
-        
-        return dp[idx] = min({day_1, day_7, day_30});
-            
+        vector<int> dp(366, INT_MAX);
+        dp[0] = 0;
+        int j = 0;
+        for (int i = 1; i < dp.size(); i++) {
+            if(i > days.back()) break;
+            if (j >= days.size() || i != days[j]) { 
+                dp[i] = dp[i - 1]; 
+                continue; 
+            }
+            dp[i] = min({dp[i], (i >= 1 ? dp[i-1] : 0) + costs[0], (i >= 7 ? dp[i-7] : 0) + costs[1], (i >= 30 ? dp[i-30] : 0) + costs[2]});
+            j++;
+        }
+
+        return dp[days.back()];
     }
 };
 
+ 
