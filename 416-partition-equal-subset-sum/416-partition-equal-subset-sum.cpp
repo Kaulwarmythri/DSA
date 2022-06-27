c5589699057fd019e79eacd55cabee21087157f1
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-06-27 07:45:50 +0530

    Time: 52 ms (92.53%), Space: 9.1 MB (97.80%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,22 +1,15 @@
 class Solution {
-    vector<vector<int>> dp;
 public:
     bool canPartition(vector<int>& nums) {
-        int sum = accumulate(nums.begin(), nums.end(), 0);
-        if(sum & 1) return false;
-        
-        dp.resize(nums.size()+1, vector<int>(sum+1, -1));
-        
-        return canDo(nums, sum/2);
-    }
-    
-    bool canDo(vector<int>& nums, int sum, int i=0) {
-        if(sum == 0) return true;
-        
-        if(sum < 0 || i >= nums.size()) return false;
-        
-        if(dp[i][sum] != -1) return dp[i][sum];
-        
-        return dp[i][sum] = canDo(nums, sum-nums[i], i+1) || canDo(nums, sum, i+1);
+        int totalSum = accumulate(begin(nums), end(nums), 0), halfSum = totalSum / 2;
+        if(totalSum & 1) return false;
+        bool dp[halfSum+1]; memset(dp, false, sizeof dp);
+        dp[0] = true;                        
+        for(int num : nums) 
+            for(int j = halfSum; j >= num; j--)
+                if(dp[j - num])
+                    dp[j] = true;
+            
+        return dp[halfSum];
     }
 };
