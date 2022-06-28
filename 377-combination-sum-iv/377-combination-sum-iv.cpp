5dde255aa40fbaffccf55af0b02a8376d73b8220
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-06-28 09:35:43 +0530

    Time: 4 ms (43.52%), Space: 6.8 MB (14.50%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -1,35 +1,20 @@
 class Solution {
 public:
-    vector<int> dp;
-    int combinationSum4(vector<int>& nums, int X) {
-        dp.resize(X+1, -1);
+    int combinationSum4(vector<int>& nums, int X) { 
+        vector<int> dp(X+1, -1);
         dp[0] = 1;
-        solve(nums, X);
-        return dp[X];
+        
+        return solve(nums, X, dp);
     }
-    int solve(vector<int> nums, int X){
+    
+    int solve(vector<int> &nums, int X, vector<int> &dp) {
         if(dp[X] != -1) return dp[X];
+        
         int ans = 0;
-        for(auto num : nums){
-            if(num <= X) ans += solve(nums, X-num);
+        
+        for(auto num: nums) {
+            if(num <= X) ans += solve(nums, X-num, dp);
         }
         return dp[X] = ans;
     }
-    
 };
-
-
-/* Recursive Solution for how to approach the above dp solution
-class Solution {
-public:
-    int combinationSum4(vector<int>& nums, int X) {
-        if(X == 0) return 1;
-        int ans = 0;
-        for(auto num : nums){
-            if(num <= X) ans += combinationSum4(nums, X-num);
-        }
-        return ans;
-    }
-};
-*/
-
