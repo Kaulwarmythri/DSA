be3ae9a7490372b40ff7ca447083749739895332
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-06-28 09:11:34 +0530

    Time: 27 ms (15.94%), Space: 10.8 MB (46.12%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.cpp
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -1,26 +1,29 @@
 class Solution {
-public:
+    vector<vector<int>> ans;
+    vector<int> buff;
     int n;
-    vector<vector<int>> combinationSum2(vector<int>& C, int X) {
-        n = C.size();
-        sort(C.begin(), C.end());
-        vector<vector<int>> ans;
-        vector<int> curr;
-        solve(C, 0, curr, ans, X);
+public:
+    vector<vector<int>> combinationSum2(vector<int>& c, int X) {
+        n = c.size();
+        sort(c.begin(), c.end());
+        
+        solve(c, 0, X, buff);
         return ans;
     }
-    void solve(vector<int> &C, int i, vector<int> &curr, vector<vector<int>> &ans, int X){
-        if(X == 0){
-            ans.push_back(curr);
+    
+    void solve(vector<int>& c, int i, int X, vector<int> &buff) {
+        if(X == 0) {
+            ans.push_back(buff);
             return;
         }
-        for(int j=i; j<n; j++){
-            if(X - C[j] >= 0){
-                if(j > i && C[j]==C[j-1]) continue;
-                curr.push_back(C[j]);
-                solve(C, j+1, curr, ans, X-C[j]);
-                curr.pop_back();
+        
+        for(int j=i; j<n; j++) {
+            if(c[j] <= X) {
+                if(j > i && c[j] == c[j-1]) continue;
+                buff.push_back(c[j]);
+                solve(c, j+1, X-c[j], buff);
+                buff.pop_back();
             }
         }
-    } 
+    }
 };
