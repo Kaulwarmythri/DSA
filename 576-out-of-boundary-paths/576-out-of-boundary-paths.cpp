f0f685126b6b313cbff4c4a4fa0489afdaf5c925
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-06-29 08:06:36 +0530

    Time: 13 ms (62.47%), Space: 6.5 MB (77.39%) - LeetHub

--- 576-out-of-boundary-paths/576-out-of-boundary-paths.cpp
+++ 576-out-of-boundary-paths/576-out-of-boundary-paths.cpp
@@ -1,23 +1,20 @@
 class Solution {
     int mod = pow(10, 9) + 7, moves[4][2] = {{0, -1}, {-1, 0}, {1, 0}, {0, 1}};
-    int dp[50][50][51] = {[0 ... 49] = {[0 ... 49] = {[0 ... 50] = -1}}};
 public:
-    bool outOfBounds(int m, int n, int r, int c) {
-	    return r < 0 || r >= m || c < 0 || c >= n;
-    }
     int findPaths(int m, int n, int maxMove, int r, int c) {        
-        bool isOut = outOfBounds(m, n, r, c);
-        
-        if(isOut || !maxMove) return isOut;
-        
-        if(dp[r][c][maxMove] != -1) return dp[r][c][maxMove];  
-        
-        dp[r][c][maxMove] = 0;   
-        
-        for(int i = 0; i < 4; i++) dp[r][c][maxMove] = (dp[r][c][maxMove] + 
-                                                        
-             findPaths(m, n, maxMove-1, r + moves[i][0], c + moves[i][1])) % mod;
-        
-        return dp[r][c][maxMove];
+        auto outOfBounds = [&](int m, int n, int r, int c){
+            return r < 0 || r >= m || c < 0 || c >= n;
+        };
+        uint dp[51][51][51]{};
+        for(int M = 1; M <= maxMove; M++) 
+            for(int i = 0; i < m; i++) 
+                for(int j = 0; j < n; j++) 
+                    for(int k = 0; k < 4; k++)       
+                        if(outOfBounds(m, n, i + moves[k][0], j + moves[k][1])) 
+                            dp[i][j][M]++;
+                        else dp[i][j][M] += dp[i + moves[k][0]][j + moves[k][1]][M-1] % mod;
+        return dp[r][c][maxMove] % mod;
     }
 };
+
+
