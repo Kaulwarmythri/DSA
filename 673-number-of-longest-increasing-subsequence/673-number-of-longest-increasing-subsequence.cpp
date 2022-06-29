0be4fdcd70e1d80d7c15408ab7b4401612f9699b
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-06-29 09:28:03 +0530

    Time: 205 ms (61.75%), Space: 13.3 MB (49.52%) - LeetHub

--- 673-number-of-longest-increasing-subsequence/673-number-of-longest-increasing-subsequence.cpp
+++ 673-number-of-longest-increasing-subsequence/673-number-of-longest-increasing-subsequence.cpp
@@ -1,22 +1,28 @@
 class Solution {
 public:
     int findNumberOfLIS(vector<int>& nums) {
-        int n = nums.size(), cnt = 0, len = 0;
-        vector<pair<int, int>> lis(n);
+        int n = nums.size(), maxLen = 1, ans = 0;
         
-        for(int i=n-1; i>=0; i--){
-            int maxLen = 1, maxCnt = 1;
-            for(int j=i+1; j<n; j++){
-                if(nums[j] > nums[i]){
-                    int l = lis[j].first, c = lis[j].second;
-                    if(l+1 > maxLen) maxLen = l+1, maxCnt = c;
-                    else if(l+1 == maxLen) maxCnt += c;
+        vector<int> lis(n, 1), cnt(n, 1);
+        
+        for(int i=1; i<n; i++) {
+            for(int j=0; j<i; j++) {
+                if(nums[j] < nums[i]) {
+                    if(lis[i] < lis[j] + 1) {
+                        lis[i] = lis[j]+1;
+                        cnt[i] = cnt[j];
+                    } else if(lis[i] == lis[j] + 1) {
+                        cnt[i] += cnt[j];
+                        
+                    }
                 }
             }
-            if(maxLen > len) len = maxLen, cnt = maxCnt;
-            else if(maxLen == len) cnt+= maxCnt;
-            lis[i] = {maxLen, maxCnt};
+            maxLen = max(maxLen, lis[i]);
         }
-        return cnt;
+        
+        for(int i=0; i<n; i++) if(lis[i] == maxLen) ans += cnt[i];
+        
+        return ans;
     }
 };
+
