06569495a115117ff242ca0088d25034b436936b
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-07-01 08:20:57 +0530

    Time: 99 ms (36.17%), Space: 31.1 MB (11.03%) - LeetHub

---
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -0,0 +1,23 @@
+class Solution {
+    vector<vector<int>> m{{1}, {0,2}, {0,1,3,4}, {2,4}, {0}, {0,1,2,3,4}};
+    int mod = pow(10, 9) + 7;
+    vector<vector<int>> dp;
+public:
+    int countVowelPermutation(int n) {
+        dp.resize(n+1, vector<int>(6, 0));
+        return solve(n);
+    }
+    
+    int solve(int n, char prev = 5) {
+        if(n == 0) return 1;
+        
+        if(dp[n][prev])return dp[n][prev];
+        
+        int ans = 0;
+        
+        for(auto &i : m[prev]) {
+            ans = (ans + solve(n-1, i))% mod;
+        }
+        return dp[n][prev] = ans;
+    }
+};
