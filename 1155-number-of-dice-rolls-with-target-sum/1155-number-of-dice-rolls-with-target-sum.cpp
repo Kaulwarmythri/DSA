899bce2885371705807cba5f61c1bfdf293490a5
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-07-01 07:43:19 +0530

    Time: 140 ms (13.70%), Space: 8.5 MB (27.18%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -1,20 +1,18 @@
-#define M 1000000007
 class Solution {
+    int mod = pow(10, 9) + 7;
 public:
     int numRollsToTarget(int n, int k, int X) {
-        
         vector<vector<int>> dp(n+1, vector<int>(X+1, 0));
         
-        dp[0][0] = 1;
+        for(int j=1; j<=X && j <= k; j++) dp[1][j] = 1;
         
-        for(int i = 1; i <= n; i++) {
-            for(int j = 1; j <= X; j++) {
-                for(int p = 1; p <= k && p <= j; p++) {
-                    dp[i][j] += (dp[i-1][j-p]);
-                    dp[i][j] %= M;
+        for(int i=2; i<=n; i++) {
+            for(int j=1; j<=X; j++) {
+                for(int m=1; m<=k && m<j; m++) {
+                    dp[i][j] = (dp[i][j] % mod + dp[i-1][j - m] % mod) % mod;
                 }
-            } 
+            }
         }
-        return dp[n][X];
+        return dp[n][X] % mod;
     }
 };
