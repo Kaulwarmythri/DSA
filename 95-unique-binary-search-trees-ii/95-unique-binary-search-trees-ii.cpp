992ad0904e570b0d98ecfb89e8b10021ac1f605b
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-07-03 10:43:45 +0530

    Time: 39 ms (14.01%), Space: 15.7 MB (66.79%) - LeetHub

--- 95-unique-binary-search-trees-ii/95-unique-binary-search-trees-ii.cpp
+++ 95-unique-binary-search-trees-ii/95-unique-binary-search-trees-ii.cpp
@@ -5,21 +5,22 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode*left, TreeNode*right) : val(x), left(left), right(right){}
- * }; */
-
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),right(right){}
+ * }; 
+ */
 class Solution {
 public:
     vector<TreeNode*> generateTrees(int n) {
-        return constructTrees(1, n);
+        return solve(1, n);
     }
     
-    vector<TreeNode *> constructTrees(int s, int e) {
-        vector<TreeNode *> ans; 
+    vector<TreeNode*> solve(int s, int e) {
+        vector<TreeNode *> ans;
         if(s > e) {
             ans.push_back(NULL);
             return ans;
         }
+        
         if(s == e) {
             TreeNode *root = new TreeNode(s);
             ans.push_back(root);
@@ -27,17 +28,17 @@ public:
         }
         
         for(int i=s; i<=e; i++) {
-            vector<TreeNode *> left = constructTrees(s, i-1);
-            vector<TreeNode *> right = constructTrees(i+1, e);
+            vector<TreeNode*> left = solve(s, i-1);
+            vector<TreeNode*> right = solve(i+1, e);
             
-            for(auto l : left) {
-                for(auto r : right) {
+            for(auto l: left) {
+                for(auto r: right) {
                     TreeNode *root = new TreeNode(i);
                     root->left = l;
                     root->right = r;
                     ans.push_back(root);
                 }
-            }   
+            }
         }
         return ans;
     }
