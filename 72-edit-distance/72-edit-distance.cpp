cb0c1fe0d2a678a4fb030dab2c9fa5e0b24bc52d
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-07-07 12:17:38 +0530

    Time: 124 ms (13.60%), Space: 81.5 MB (6.50%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,19 +1,28 @@
 class Solution {
+    int m, n;
+    vector<vector<int>> dp;
 public:
-    int minDistance(string s1, string s2) {
-        int m = s1.size(), n = s2.size();
+    int minDistance(string word1, string word2) {
+        m = word1.size(); n = word2.size();
+        dp.resize(m+1, vector<int>(n+1, -1));
         
-        vector<vector<int>> dp(m+1, vector<int>(n+1));
-        
-        for(int i=0; i<=m; i++) {
-            for(int j=0; j<=n; j++) {
-                if(i == 0 || j == 0) dp[i][j] = i + j;
-                else {
-                    dp[i][j] = (s1[i-1] == s2[j-1]) ? dp[i-1][j-1] 
-                        : 1 + min({dp[i][j-1], dp[i-1][j], dp[i-1][j-1]});
-                }
-            }
-        }
-        return dp[m][n];
+        return solve(word1, word2, m, n, dp);
     }
+    
+    int solve(string s1, string s2, int m, int n, vector<vector<int>>&dp) {
+        
+        if(!m) dp[m][n] = n;
+        
+        if(!n) dp[m][n] = m;
+        
+        if(dp[m][n] != -1) return dp[m][n];
+        
+        if(s1[m-1] == s2[n-1]) dp[m][n] = solve(s1, s2, m-1, n-1, dp);
+        
+        else {
+            dp[m][n] = 1 + min({solve(s1, s2, m-1, n-1, dp), solve(s1, s2, m-1, n, dp), solve(s1, s2, m, n-1, dp)});
+        }
+        return dp[m][n];;
+    }
+    
 };
