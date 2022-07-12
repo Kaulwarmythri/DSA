870073635e8d27c627a41b9de7eba9ee8c3365c1
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-07-12 08:30:38 +0530

    Time: 32 ms (8.53%), Space: 17.6 MB (47.49%) - LeetHub

--- 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.cpp
+++ 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.cpp
@@ -1,66 +1,33 @@
-/*
-// Definition for a Node.
-class Node {
+/* // Definition for a Node.
+class Node{
 public:
     int val;
     Node* left;
     Node* right;
     Node* next;
-
     Node() : val(0), left(NULL), right(NULL), next(NULL) {}
-
     Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
-
     Node(int _val, Node* _left, Node* _right, Node* _next)
         : val(_val), left(_left), right(_right), next(_next) {}
-};
-*/
+};*/
 
 class Solution {
 public:
     Node* connect(Node* root) {
         if(!root) return NULL;
         
-        if(root->left) root->left->next = root->right ? root->right : findNext(root->next);
+        queue<Node *> q; q.push(root);
         
-        if(root->right) root->right->next = findNext(root->next);
-        
-        connect(root->right);
-        
-        connect(root->left);
-        
+        while(!q.empty()) {
+            int s = q.size();
+            while(s--) {
+                auto curr = q.front(); q.pop();
+                if(curr->left) q.push(curr->left);
+                if(curr->right) q.push(curr->right);
+                
+                curr->next = (s >= 1) ? q.front() : NULL;
+            }
+        }
         return root;
     }
-    
-    Node *findNext(Node *root) {
-        if(!root) return NULL;
-        if(root->left) return root->left;
-        
-        if(root->right) return root->right;
-        
-        return findNext(root->next);
-    }
 };
-
-
-
-//  Node* connect(Node* root) {
-//         if(!root) return NULL;
-        
-//         if(root->left) root->left->next = root->right ? root->right : find(root->next);
-        
-//         if(root->right) root->right->next = find(root->next);
-        
-//         connect(root->right);
-//         connect(root->left);
-        
-//         return root;
-//     }
-    
-//     Node *find(Node *root){
-//         if(!root) return NULL;
-//         if(root->left) return root->left;
-//         if(root->right) return root->right;
-        
-//         return find(root->next);
-//     }
