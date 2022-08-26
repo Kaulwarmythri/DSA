b6e15cc9688b4492cd5cdc9e96e37084cddd3853
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-08-26 13:09:25 +0530

    Time: 20 ms (82.24%), Space: 16.7 MB (54.04%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int n = nums.size(), max_reach = 0, curr_reach = 0, jumps = 0;
+        int n = nums.size(), curr_reach = 0, max_reach = 0, jumps = 0;
         
         for(int i=0; i<n-1; i++) {
             max_reach = max(max_reach, nums[i] + i);
