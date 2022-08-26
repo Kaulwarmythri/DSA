5dcdf044988ecac78d06431531f9036f059d5237
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-08-26 11:45:33 +0530

    Time: 11 ms (21.37%), Space: 11.2 MB (29.48%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -4,29 +4,20 @@ public:
         int n = nums.size(), l = 0, r = n-1;
         
         while(l <= r) {
-            int mid = (l + r)/2; 
+            int mid = l + (r-l) / 2;
             
-            if(nums[mid] == X) return mid; 
+            if(nums[mid] == X) return mid;
             
             if(nums[l] <= nums[mid]) {
-                if(X >= nums[l] && X <= nums[mid]) {
-                    r = mid - 1; 
-                }
-                else {
-                    l = mid + 1; 
-                }
-            }
-            else {
-                if(X >= nums[mid] && X <= nums[r]) {
-                    l = mid + 1; 
-                }
-                else {
-                    r = mid - 1; 
-                }
+                if(X >= nums[l] && X <= nums[mid]) r = mid -1;
+                
+                else l = mid+1;
+            } else {
+                if(X >= nums[mid] && X <= nums[r]) l = mid+1;
+                
+                else r = mid-1;
             }
         }
-        
         return -1;
     }
-    
 };
