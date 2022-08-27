51b074262c43d83e58ecf2bea19c1e1a6ecec2b5
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-08-27 12:15:28 +0530

    Time: 190 ms (38.44%), Space: 67.7 MB (54.19%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -4,9 +4,9 @@ public:
         int n = nums.size(), sum = 0, maxi = INT_MIN;
         
         for(int i=0; i<n; i++) {
-            sum += nums[i];
+            sum = max(nums[i], nums[i]+sum);
             maxi = max(maxi, sum);
-            if(sum <= 0) sum = 0;
+            
         }
         return maxi;
     }
