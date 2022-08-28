e592edfaa37fb3d859ef50807df119c162d21956
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-08-28 12:47:17 +0530

    Time: 23 ms (11.76%), Space: 12.1 MB (25.18%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,28 +1,15 @@
 class Solution {
-    vector<vector<int>> dp;
-    int n;
 public:
     int maxProfit(vector<int>& prices) {
-        n = prices.size(); 
-        dp.resize(n+1, vector<int>(2, -1));
-        
-        return solve(prices, 0, true);
-    }
-    
-    int solve(vector<int> &prices, int i, bool can) {
-        if(i >= n) return 0;
-        
-        if(dp[i][can] != -1) return dp[i][can];
-        
-        if(can) {
-            int buy = solve(prices, i+1, false) - prices[i];
-            int cooldown = solve(prices, i+1, true);
-            return dp[i][can] = max(buy, cooldown);
-        } else {
-            int sell = solve(prices, i+2, true) + prices[i];
-            int cooldown = solve(prices, i+1, false);
-            return dp[i][can] = max(sell, cooldown);
+        if(prices.size() < 2) return 0;
+        int n = prices.size(), buy, sell, profit = 0, i = 0;
+        while(i < n-1) {
+            while(i < n-1 && prices[i] >= prices[i+1]) i++;
+            buy = prices[i];
+            while(i < n-1 && prices[i] <= prices[i+1]) i++;
+            sell = prices[i];
+            profit += sell - buy;
         }
-        return 0;
+        return profit;
     }
 };
