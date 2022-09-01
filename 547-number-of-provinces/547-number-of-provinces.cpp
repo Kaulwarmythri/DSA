d7afb78dd9e7a17450010cb27fb518a1251e5922
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-09-01 12:36:57 +0530

    Time: 54 ms (19.22%), Space: 13.9 MB (48.56%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.cpp
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -1,27 +1,62 @@
-class Solution {
+class DSU {
+public:
     int n;
+    vector<int> parent, rank;
+    
+    DSU(int n) {
+        this->n = n;
+        parent.resize(n);
+        
+        for(int i=0; i<parent.size(); i++)
+            parent[i] = i;
+        
+        rank.resize(n, 0);
+    }
+    
+    int find(int i) {
+        if(parent[i] == i) 
+            return i;
+        return parent[i] = find(parent[i]);
+    }
+    
+    void unite(int x, int y) {
+        int xrep = find(x), yrep = find(y);
+        
+        if(rank[xrep] >= rank[yrep])
+            parent[yrep] = xrep;
+        else 
+            parent[xrep] = yrep;
+    }
+    
+};
+
+class Solution {
 public:
     int findCircleNum(vector<vector<int>>& G) {
-        n = G.size();
-        int count = 0;
-        vector<bool> vis(n, 0);
+        int n = G.size();
+        DSU *dsu = new DSU(n);
         
         for(int i=0; i<n; i++) {
-            if(!vis[i]) {
-                count++;
-                dfs(G, vis, i);
+            for(int j=i; j<n; j++) {
+                if(G[i][j])
+                    dsu->unite(i, j);
+            }
+        }
+        
+        unordered_map<int, int> m;
+        int count = 0;
+        
+        for(int i=0; i<n; i++) {
+            for(int j=i; j<n; j++) {
+                if(G[i][j]) {
+                    if(dsu->find(i) == dsu->find(j) && m.find(dsu->find(i)) == m.end()) {
+                        count++;
+                        m[dsu->find(i)] = 1;
+                    }
+                }
+                    
             }
         }
         return count;
-    }
-    
-    void dfs(vector<vector<int>> &G, vector<bool> &vis, int curr) {
-        vis[curr] = true;
-        
-        for(int j=0; j<n; j++) {
-            if(G[curr][j] && !vis[j]) {
-                dfs(G, vis, j);
-            }
-        }
     }
 };
