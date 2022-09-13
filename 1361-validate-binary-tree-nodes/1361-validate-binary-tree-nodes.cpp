8fff37f162a07617371a653c7063710abaeec654
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-09-13 14:06:10 +0530

    Time: 86 ms (60.21%), Space: 32.2 MB (84.12%) - LeetHub

--- 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.cpp
+++ 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    bool validateBinaryTreeNodes(int n, vector<int>& leftc, vector<int>& rightc) {
-        vector<int> parentFreq(n);
+    bool validateBinaryTreeNodes(int n, vector<int>& leftC, vector<int>& rightC) {
+        vector<int> parentFreq(n, 0);
         
         for(int i=0; i<n; i++) {
-            if((leftc[i] != -1 && ++parentFreq[leftc[i]] > 1) || rightc[i] != -1 && ++parentFreq[rightc[i]] > 1) 
+            if(leftC[i] != -1 && ++parentFreq[leftC[i]] > 1 || rightC[i] != -1 && ++parentFreq[rightC[i]] > 1)
                 return false;
         }
         
         int root = -1;
-        
         for(int i=0; i<n; i++) {
             if(!parentFreq[i]) {
-                if(root == -1) root = i;
-                else return false;
+               if(root == -1) root = i;
+                else return false; 
             }
         }
-
-        return countNodes(root, leftc, rightc) == n;
+        
+        return countNodes(root, leftC, rightC) == n;
+        
     }
     
-    int countNodes(int root, vector<int> &leftc, vector<int> &rightc) {
+    int countNodes(int root, vector<int> &leftC, vector<int> &rightC) {
         if(root == -1) return 0;
-        return 1 + countNodes(leftc[root], leftc, rightc) + countNodes(rightc[root], leftc, rightc);
+        return 1 + countNodes(leftC[root], leftC, rightC) + countNodes(rightC[root], leftC, rightC);
     }
 };
