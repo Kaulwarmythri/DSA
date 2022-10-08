408fac1155a4513fe74fcb3fe82d09bea29c6773
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-10-08 12:56:03 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (86.05%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.cpp
+++ 1922-count-good-numbers/1922-count-good-numbers.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     int countGoodNumbers(long long n) {
-        int mod = 1000000007;
-        return modPow(5, (n+1)/2, mod) * modPow(4, n/2, mod) % mod;
+        int mod = 1e9 + 7;
+        return modpow(5, (n+1)/2, mod) * modpow(4, n/2, mod) % mod;
     }
     
-    long long modPow(long long a, long long b, int mod) {
-        if(b == 0) return 1;
+    long long modpow(long long a, long long b, int mod) {
+        if(!b) return 1;
         
-        auto p = modPow(a, b/2, mod);
-        
+        auto p = modpow(a, b/2, mod);
         return p * p * (b%2 ? a : 1) % mod;
-    }
+    } 
 };
