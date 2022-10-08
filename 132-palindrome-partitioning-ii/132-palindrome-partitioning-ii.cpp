04ba2cc13f06c6a1f98ac20d4e0d3c6762d3d59d
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-10-08 13:29:51 +0530

    Time: 192 ms (70.87%), Space: 337.9 MB (5.01%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
@@ -1,29 +1,37 @@
 class Solution {
-    vector<vector<int>> dp;
-	vector<vector<int>> dp1;
+    vector<vector<int>> dp, pals;
 public:
-	bool isPalindrome(string& s, int i, int j) {
-		if (i >= j) return true;
-		if (dp1[i][j] != -1) return dp1[i][j];
-		if (s[i] == s[j]) return dp1[i][j] = isPalindrome(s, i + 1, j - 1);
-		return dp1[i][j] = false;
+	bool isPalindrome(string &s, int i, int j) {
+		if (i >= j) 
+            return true;
+		if (pals[i][j] != -1) 
+            return pals[i][j];
+		if (s[i] == s[j]) 
+            return pals[i][j] = isPalindrome(s, i + 1, j - 1);
+		return pals[i][j] = false;
 	}
 
-	int solve(string s, int l, int r){
-		if(l>=r) return dp[l][r] = 0;
-		if(dp[l][r]!=-1) return dp[l][r];
-		if(isPalindrome(s,l,r)) return dp[l][r] = 0;
+	int solve(string s, int i, int j){
+		if(i >= j) 
+            return dp[i][j] = 0;
+		if(dp[i][j] != -1) 
+            return dp[i][j];
+		if(isPalindrome(s, i, j)) 
+            return dp[i][j] = 0;
+        
 		int ans = INT_MAX;
-		for(int i=l;i<=r-1;i++){
-			if(isPalindrome(s,l,i)) ans = min(ans, 1+solve(s,i+1,r));
+		for(int k=i; k<j; k++){
+			if(isPalindrome(s, i, k)) ans = min(ans, 1+solve(s, k+1, j));
 		}
-		return dp[l][r] = ans;
+		return dp[i][j] = ans;
 	}
 
 	int minCut(string s) {
-		dp.resize(s.size(),vector<int> (s.size(),-1));
-		dp1.resize(s.size(),vector<int> (s.size(),-1));
-		return solve(s,0,s.size()-1);
+        int n = s.size();
+		dp.resize(n, vector<int> (n, -1));
+		pals.resize(n, vector<int>(n, -1));
+                  
+		return solve(s, 0, n-1);
 	}
 };
 
