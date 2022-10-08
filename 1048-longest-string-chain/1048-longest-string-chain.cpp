cf16f8fb58057c1b706f19efe03f6fbdc1bcf0da
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-10-08 11:55:24 +0530

    Time: 322 ms (17.28%), Space: 23.3 MB (24.55%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-    int longestStrChain(vector<string>& words) {
+   int longestStrChain(vector<string>& words) {
         int n = words.size();
-        
-        sort(words.begin(), words.end(), [](string a, string b)-> bool {
+        sort(words.begin(), words.end(), [](string a, string b) -> bool {
             return a.size() < b.size();
         });
         
-        vector<int> dp(n, 1);
+        vector<int> lis(n, 1);
         
         for(int i=1; i<n; i++) {
             for(int j=0; j<i; j++) {
-                if(words[j].size()+1 == words[i].size() && check(words[j], words[i]) && dp[i] < dp[j] + 1) {
-                    dp[i] = dp[j] + 1;
+                if(words[j].size() + 1 == words[i].size() && check(words[j], words[i]) && lis[i] < lis[j] + 1) {
+                    lis[i] = lis[j] + 1;
                 }
             }
         }
-        return *max_element(dp.begin(), dp.end());
+        return *max_element(lis.begin(), lis.end());
     }
-    
+
     bool check(string a, string b) {
         int m = a.size(), n = b.size(), missing = 0, i=0, j=0;
         
