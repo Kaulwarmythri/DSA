e5119a9da9420832cac9b8567635ed327d570e6d
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-10-08 13:10:00 +0530

    Time: 3 ms (39.66%), Space: 6 MB (35.63%) - LeetHub

--- 1969-minimum-non-zero-product-of-the-array-elements/1969-minimum-non-zero-product-of-the-array-elements.cpp
+++ 1969-minimum-non-zero-product-of-the-array-elements/1969-minimum-non-zero-product-of-the-array-elements.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
-    long long myPow(long long base, long long exp, int mod) {
-        if (exp == 0) return 1;
-        if (exp == 1) return base % mod;
-        
-        long long ans = myPow(base, exp/2, mod);
-        
-        return (((ans * ans) % mod) * ((exp % 2 ) ? (base % mod) : 1)) % mod;
-    }
-    
     int minNonZeroProduct(int p) {
         long long range = pow(2, p);
+        int mod = 1e9+7;
         range--;
-        int mod = pow(10, 9) + 7;
         
-        long long ans = myPow(range-1, range/2, mod);
-        
-        return (ans * (range%mod)) % mod;
+        long long ans = modpow(range-1, range / 2, mod);
+        return ans * (range % mod) % mod;
+    }
+    
+    long long modpow(long long a, long long b, int mod) {
+        if(!b) return 1;
+        if(b == 1) return a % mod;
+        auto p = modpow(a, b/2, mod);
+        return (p * p) % mod * (b % 2 ? a % mod : 1) % mod;
     }
 };
 
