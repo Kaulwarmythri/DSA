25e951a5f47d931558576068442e22fdae9d1e34
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-10-10 09:37:32 +0530

    Time: 5 ms (26.41%), Space: 6.1 MB (28.61%) - LeetHub

---
+++ 1576-replace-all-s-to-avoid-consecutive-repeating-characters/1576-replace-all-s-to-avoid-consecutive-repeating-characters.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string modifyString(string s) {
+        int n = s.size();
+        
+        for(int i=0; i<n; i++) {
+            if(s[i] == '?') {
+                for(s[i] = 'a'; s[i] <= 'c'; s[i]++) {
+                    if((i == 0 || s[i] != s[i-1]) && (i == n-1 || s[i] != s[i+1]))
+                        break;
+                }
+            }
+        }
+        return s;
+    }
+};
