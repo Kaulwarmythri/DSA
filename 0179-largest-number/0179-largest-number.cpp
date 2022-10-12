6b33a5681f82c4dab71a7a1a1ba7773474cec372
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-10-12 08:10:31 +0530

    Time: 7 ms (91.79%), Space: 11.5 MB (48.56%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -3,9 +3,13 @@ public:
     string largestNumber(vector<int>& nums) {
         vector<string> A;
         
-        for(auto &i: nums) 
-            A.push_back(to_string(i));
+        for(auto &i: nums) A.push_back(to_string(i));
+        
         sort(A.begin(), A.end(), [](string &a, string &b) {
+            int len = min(a.size(), b.size());
+            for(int i=0; i<len; i++) {
+                if(a[i] != b[i]) return a[i] > b[i];
+            }
             return a+b > b+a;
         });
         
