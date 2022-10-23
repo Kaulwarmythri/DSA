02cb20570646529f4ad8e086b13bebd3cd1ede1b
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-10-23 17:37:22 +0530

    Time: 89 ms (37.59%), Space: 14.9 MB (68.28%) - LeetHub

---
+++ 1340-jump-game-v/1340-jump-game-v.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    int n;
+    vector<int> dp;
+public:
+    int maxJumps(vector<int>& arr, int d) {
+        n = arr.size();
+        dp.resize(n+1, -1);
+        
+        int ans = INT_MIN;
+        for(int i=0; i<n; i++) {
+            ans = max(ans, 1 + solve(arr, i, d));
+        }
+        return ans;
+    }
+    
+    int solve(vector<int> &arr, int i, int d) {
+        if(dp[i] != -1) return dp[i];
+        
+        dp[i] = 0;
+        for(int j=i+1; j<n && arr[j] < arr[i] && j <= i+d; j++) {
+            dp[i] = max(dp[i], 1 + solve(arr, j, d));
+        }
+        for(int j=i-1; j>=0 && arr[j] < arr[i] && j >= i-d; j--) {
+            dp[i] = max(dp[i], 1 + solve(arr, j, d));
+        }
+        return dp[i];
+    }
+};
