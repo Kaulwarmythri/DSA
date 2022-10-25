26df32e6acfd494e21962872f8a2d5970e7b3198
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-10-25 08:40:09 +0530

    Time: 1682 ms (22.02%), Space: 48.2 MB (16.78%) - LeetHub

--- 1761-minimum-degree-of-a-connected-trio-in-a-graph/1761-minimum-degree-of-a-connected-trio-in-a-graph.cpp
+++ 1761-minimum-degree-of-a-connected-trio-in-a-graph/1761-minimum-degree-of-a-connected-trio-in-a-graph.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     int minTrioDegree(int n, vector<vector<int>>& edges) {
-        vector<vector<int>> M(n, vector<int>(n));
+        vector<set<int>> G(n);
+        vector<int> cnt(n, 0);
         
-        vector<int> count(n);
         int ans = INT_MAX;
         
         for(auto &e: edges) {
-            M[min(e[0], e[1]) - 1][max(e[0], e[1]) - 1] = 1;
+            G[min(e[0], e[1]) - 1].insert(max(e[0], e[1]) - 1);
             
-            count[e[0] - 1]++;
-            count[e[1] - 1]++;
+            cnt[e[0] - 1]++;
+            cnt[e[1] - 1]++;
         }
         
         for(int i=0; i<n; i++) {
-            for(int j=i+1; j<n; j++) {
-                if(M[i][j]) {
-                    for(int k=j+1; k<n; k++) {
-                        if(M[i][k] && M[j][k]) {
-                            ans = min(ans, count[i] + count[j] + count[k] - 6);
-                        }
+            for(auto j=G[i].begin(); j != G[i].end(); j++) {
+                for(auto k=next(j); k != G[i].end(); k++) {
+                    if(G[*j].count(*k)) {
+                        ans = min(ans, cnt[i] + cnt[*j] + cnt[*k] - 6);
                     }
                 }
             }
