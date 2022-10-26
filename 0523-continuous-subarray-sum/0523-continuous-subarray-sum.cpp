046162641367d260bafc4ba95f1b4a4451e63c40
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-10-26 07:44:32 +0530

    Time: 468 ms (51.76%), Space: 113.7 MB (71.68%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -1,32 +1,23 @@
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
-        if(nums.size()<2)
-            return false;
-       unordered_map<int, int> mp;
-        mp[0]=-1;
+        if(nums.size() < 2) return false;
+        unordered_map<int, int> m; m[0] = -1;
         
-        int runningSum=0;
+        int prefix = 0;
         
-        for(int i=0; i<nums.size();i++) {
-            runningSum+=nums[i];
+        for(int i=0; i<nums.size(); i++) {
+            prefix += nums[i];
+            if(k != 0)
+                prefix %= k;
             
-            if(k!=0) 
-                runningSum = runningSum%k;
-            
-            if(mp.find(runningSum)!=mp.end()) { 
-                if(i-mp[runningSum]>1)
+            if(m.find(prefix) != m.end()) {
+                if(i - m[prefix] > 1)
                     return true;
+            } else {
+                m[prefix] = i;
             }
-            else mp[runningSum]=i;       
         }
-        
         return false;
     }
 };
-
-
-
-
-//m[0] = -1 will help if for the last element the sum becomes a multiple of 6 ==> Eg. [1, 2, 3]
-//It can also prevent returning true if only 1 element (Eg. element at 0th index) is a multiple of k (Since 0 - (-1) != > 1).
