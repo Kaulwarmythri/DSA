3be31e964a52193c0f6536b1566370f9908158d9
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-11-03 07:21:52 +0530

    Time: 722 ms (60.14%), Space: 168.5 MB (16.83%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -1,53 +1,54 @@
 class Solution {
 public:
-    int longestPalindrome(vector<string>& words) { 
-        unordered_map<string, int> m;
-        int unpaired = 0, ans = 0;
-        for (string w: words) {
-            if (w[0] == w[1]) {
-                if (m[w] > 0) {
-                    unpaired--;
-                    m[w]--;
-                    ans += 4;
-                }
-                else {
-                    m[w]++;
-                    unpaired++;
-                }
+    int longestPalindrome(vector<string>& words) {
+        vector<vector<int>> dp(26, vector<int>(26, 0));
+        int ans = 0;
+        for(auto &w: words) {
+            int a = w[0] - 'a', b = w[1] - 'a';
+            if(dp[b][a]) {
+                ans += 4;
+                dp[b][a]--;
             }
-            else {
-                string rev = w;
-                reverse(rev.begin(), rev.end());
-                if (m[rev] > 0) {
-                    ans += 4;
-                    m[rev]--;
-                }
-                else m[w]++;
-            }
+            else dp[a][b]++;
         }
-        if (unpaired > 0) ans += 2;
+        
+        for(int i=0; i<26; i++) {
+            if(dp[i][i]) {
+                ans += 2;
+                break;
+            }
+            
+        }
         return ans;
     }
 };
 
 
-// vector<vector<int>> dp(26, vector<int>(26, 0));
-//         int ans = 0;
-//         for(auto &w: words) {
-//             int a = w[0] - 'a', b = w[1] - 'a';
-//             if(dp[b][a]) {
-//                 ans += 4;
-//                 dp[b][a]--;
-//             }
-//             else dp[a][b]++;
-//         }
-        
-//         for(int i=0; i<26; i++) {
-//             if(dp[i][i]) {
-//                 ans += 2;
-//                 break;
-//             }
-            
-//         }
-        
-//         return ans;
+int longestPalindrome(vector<string>& words) { 
+    unordered_map<string, int> m;
+    int unpaired = 0, ans = 0;
+    for (string w: words) {
+        if (w[0] == w[1]) {
+            if (m[w] > 0) {
+                unpaired--;
+                m[w]--;
+                ans += 4;
+            }
+            else {
+                m[w]++;
+                unpaired++;
+            }
+        }
+        else {
+            string rev = w;
+            reverse(rev.begin(), rev.end());
+            if (m[rev] > 0) {
+                ans += 4;
+                m[rev]--;
+            }
+            else m[w]++;
+        }
+    }
+    if (unpaired > 0) ans += 2;
+    return ans;
+}
