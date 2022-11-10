65af639680232dd4f5ae8ea66c8ee0a239060659
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-11-10 07:22:31 +0530

    Time: 66 ms (46.89%), Space: 10.6 MB (58.24%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
-    string removeDuplicates(string s, int k) {
-        int n = s.size();
-        if(n<k) return s;
+    string removeDuplicates(string str, int k) {
+        int n = str.size();
+        if(k > n) return str;
         
-        stack<pair<char,int>> stk;
+        stack<pair<char,int>> s;
         
         for(int i=0; i<n; ++i){
             
-            if(stk.empty() || stk.top().first != s[i]) stk.push({s[i],1});
+            if(s.empty() || s.top().first != str[i]) s.push({str[i],1});
             
             else {
-                auto prev = stk.top();
-                stk.pop();
-                stk.push({s[i], prev.second+1});
+                auto prev = s.top(); s.pop();
+                s.push({str[i], prev.second+1});
             }
-            if(stk.top().second==k) stk.pop();
+            if(s.top().second == k) s.pop();
         }
         
         string ans = "";
-        while(!stk.empty()){
-            auto cur = stk.top();
-            stk.pop();
-            while(cur.second--){
+        while(!s.empty()){
+            auto cur = s.top(); s.pop();
+            
+            while(cur.second--)
                 ans.push_back(cur.first);
-            }
         }
         reverse(ans.begin(), ans.end());
         return ans;
     }
 };
+
+
