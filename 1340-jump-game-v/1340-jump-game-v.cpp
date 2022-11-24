2765d03584fd97aa3265dd14dc1ea8e4f72ecb04
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-11-24 21:37:38 +0530

    Time: 113 ms (22.20%), Space: 14.9 MB (69.02%) - LeetHub

--- 1340-jump-game-v/1340-jump-game-v.cpp
+++ 1340-jump-game-v/1340-jump-game-v.cpp
@@ -8,21 +8,21 @@ public:
         
         int ans = INT_MIN;
         for(int i=0; i<n; i++) {
-            ans = max(ans, 1 + solve(arr, i, d));
+            ans = max(ans, 1+solve(arr, i, d));
         }
         return ans;
     }
-    
     int solve(vector<int> &arr, int i, int d) {
         if(dp[i] != -1) return dp[i];
         
         dp[i] = 0;
-        for(int j=i+1; j<n && arr[j] < arr[i] && j <= i+d; j++) {
+        for(int j=i-1; j>=0 && arr[i] > arr[j] && j >= i-d; j--) {
             dp[i] = max(dp[i], 1 + solve(arr, j, d));
         }
-        for(int j=i-1; j>=0 && arr[j] < arr[i] && j >= i-d; j--) {
+        
+        for(int j=i+1; j<n && arr[i] > arr[j] && j <= i+d; j++) {
             dp[i] = max(dp[i], 1 + solve(arr, j, d));
         }
         return dp[i];
-    }
+    } 
 };
