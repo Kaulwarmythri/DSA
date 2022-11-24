21ab18fd999fb50087c1e96bada58b3f567d025f
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-11-24 21:19:49 +0530

    Time: 392 ms (73.03%), Space: 70.4 MB (97.93%) - LeetHub

--- 1345-jump-game-iv/1345-jump-game-iv.cpp
+++ 1345-jump-game-iv/1345-jump-game-iv.cpp
@@ -3,6 +3,7 @@ public:
     int minJumps(vector<int>& arr) {
         int n = arr.size(), jumps = 0;
         if(n == 1) return 0;
+        
         unordered_map<int, vector<int>> m;
         
         for(int i=0; i<n; i++) m[arr[i]].push_back(i);
@@ -10,15 +11,19 @@ public:
         queue<int> q; q.push(0);
         
         while(q.size()) {
-            int s = q.size(); jumps++;
+            int s = q.size();
+            jumps++;
+            
             while(s--) {
                 auto i = q.front(); q.pop();
-                if(i-1 > 0 && m.find(arr[i-1]) != m.end()) 
-                    q.push(i-1);
+                
                 if(i+1 < n && m.find(arr[i+1]) != m.end()) {
                     if(i+1 == n-1) return jumps;
                     q.push(i+1);
                 }
+                if(i-1 > 0 && m.find(arr[i-1]) != m.end()) 
+                    q.push(i-1);
+                
                 if(m.find(arr[i]) != m.end()) {
                     for(auto &x: m[arr[i]]) {
                         if(x != i) {
@@ -33,4 +38,5 @@ public:
         return jumps;
     }
 };
+
 
