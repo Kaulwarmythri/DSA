bf34715fd82109a63265a342d58acbad98af416a
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-11-25 10:12:57 +0530

    Time: 813 ms (5.87%), Space: 135.6 MB (14.60%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.cpp
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -2,13 +2,22 @@ class Solution {
 public:
     int maxResult(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<int> dp(n, INT_MIN);
-        multiset<int> s({dp[0] = nums[0]});
+        vector<int> dp(n, INT_MIN); 
+        multiset<int, greater<int>> s({dp[0] = nums[0]});
         
         for(int i=1; i<n; i++) {
             if(i > k) s.erase(s.find(dp[i-k-1]));
-            s.insert(dp[i] = nums[i] + *s.rbegin());
+            dp[i] = *s.begin() + nums[i];
+            s.insert(dp[i]);
         }
         return dp[n-1];
     }
 };
+
+// vector<int> dp(size(nums), INT_MIN);
+//     multiset<int> s ({ dp[0] = nums[0] });         // set dp[0] = nums[0] and insert it into set
+// 	for(int i = 1; i < size(nums); i++) {
+//         if(i > k) s.erase(s.find(dp[i - k - 1]));  // erase elements from which we cant jump to current index
+//         s.insert(dp[i] = *rbegin(s) + nums[i]);    // choose element with max score and jump from that to the current index
+//     }
+// 	return dp.back();
