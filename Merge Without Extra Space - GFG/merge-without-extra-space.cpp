a1c00d5114d265cda7e6e68c9420b32e3ee6911b
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-11-25 20:16:05 +0530

    Added solution - LeetHub

--- Merge Without Extra Space - GFG/merge-without-extra-space.cpp
+++ Merge Without Extra Space - GFG/merge-without-extra-space.cpp
@@ -12,21 +12,22 @@ class Solution{
     }
     
 public:
-    void merge(long long arr1[], long long arr2[], int n, int m) { 
-        long long i, j, gap = n + m;
-        for (gap = nextGap(gap); gap > 0; gap = nextGap(gap)) {
-            for (i = 0; i + gap < n; i++)
-                if (arr1[i] > arr1[i + gap])
-                    swap(arr1[i], arr1[i + gap]);
- 
-            for (j = gap > n ? gap - n : 0; i < n && j < m; i++, j++)
-                if (arr1[i] > arr2[j])
-                    swap(arr1[i], arr2[j]);
- 
-            if (j < m) {
-                for (j = 0; j + gap < m; j++)
-                    if (arr2[j] > arr2[j + gap])
-                        swap(arr2[j], arr2[j + gap]);
+    void merge(long long arr1[], long long arr2[], int m, int n) { 
+        long long i, j, gap = nextGap(m + n);
+        
+        for(gap; gap>0; gap = nextGap(gap)) {
+            for(i = 0; i + gap < m; i++) {
+                if(arr1[i] > arr1[i + gap]) swap(arr1[i], arr1[i + gap]);
+            }
+            
+            for(j = gap > m ? gap - m : 0; i < m && j < n; i++, j++) {
+                if(arr1[i] > arr2[j]) swap(arr1[i], arr2[j]);
+            }
+            
+            if(j < n) {
+                for(j = 0; j + gap<n; j++) {
+                    if(arr2[j] > arr2[j + gap]) swap(arr2[j], arr2[j + gap]);
+                }
             }
         }
     } 
