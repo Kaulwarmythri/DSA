54c8c3f343452be8115b118d8887d7cb5d1fb158
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-11-26 17:57:25 +0530

    Time: 498 ms (16.94%), Space: 72.3 MB (13.15%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
@@ -1,10 +1,11 @@
 class Solution {
-    map<int, int> dp;
+    unordered_map<int, int> dp;
+    vector<vector<int>> jobs;
     int n;
 public:
     int jobScheduling(vector<int>& S, vector<int>& E, vector<int>& P) {
         n = S.size();
-        vector<vector<int>> jobs;
+        
         
         for(int i=0; i<n; i++) {
             jobs.push_back({S[i], E[i], P[i]});
@@ -12,20 +13,20 @@ public:
         
         sort(jobs.begin(), jobs.end());
         
-        return solve(jobs, 0);
+        return solve(0);
     }
     
-    int solve(vector<vector<int>> &jobs, int curr) {
+    int solve(int curr) {
         if(curr == n) return 0;
         
         if(dp.find(curr) != dp.end()) return dp[curr];
         
-        int next = findNext(jobs, curr), inc = jobs[curr][2] + (next == -1 ? 0 : solve(jobs, next)), exc = solve(jobs, curr+1);
+        int next = findNext(curr), inc = jobs[curr][2] + (next == -1 ? 0 : solve(next)), exc = solve(curr+1);
         
         return dp[curr] = max(inc, exc); 
     }
     
-    int findNext(vector<vector<int>> &jobs, int curr) {
+    int findNext(int curr) {
         for(int next = curr+1; next < n; next++) {
             if(jobs[next][0] >= jobs[curr][1])
                 return next;
