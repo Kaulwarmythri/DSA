35492d9ad6e8fd224598a16859c8e6bc757d2727
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2022-12-31 16:20:17 +0530

    Added solution - LeetHub

--- Count the Reversals - GFG/count-the-reversals.cpp
+++ Count the Reversals - GFG/count-the-reversals.cpp
@@ -16,19 +16,19 @@ int main()
 // Contributed By: Pranay Bansal
 // } Driver Code Ends
 int countRev (string s) {
-    int n = s.size(), open = 0, close = 0;
+    int n = s.size(), temp = 0, ans = 0;
     if(n & 1) return -1;
     
     for(int i=0; i<n; i++) {
-        if(s[i] == '{') open++;
+        if(s[i] == '{') temp++;
         else {
-            if(open > 0) open--;
-            else close++;
+            if(temp == 0) {
+                temp++;
+                ans++;
+            }
+            else temp--;
         }
     }
-    int ans = open / 2 + close / 2;
-    
-    close %= 2;
-    if(close) ans += 2;
+    ans += temp / 2;
     return ans;
 }
