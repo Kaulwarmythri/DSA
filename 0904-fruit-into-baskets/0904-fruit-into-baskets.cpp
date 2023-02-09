3aaa13c28a406a340511aee83e40f3c827950c35
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2023-02-09 12:25:08 +0530

    Time: 164 ms (80.23%), Space: 71.2 MB (47.78%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
-    int totalFruit(vector<int>& tree) {
-        unordered_map<int, int> count;
-        int i, j;
-        for (i = 0, j = 0; j < tree.size(); ++j) {
-            count[tree[j]]++;
-            if (count.size() > 2) {
-                if (--count[tree[i]] == 0)count.erase(tree[i]);
+    int totalFruit(vector<int>& A) {
+        unordered_map<int, int> m;
+        
+        int n = A.size(), i = 0, j = 0;
+        for(; j<n; j++) {
+            m[A[j]]++;
+            if(m.size() > 2) {
+                if(--m[A[i]] == 0) m.erase(A[i]);
                 i++;
             }
         }
         return j - i;
     }
 };
+
+
+//Longest length of a subarray with atmost 2 different numbers
