f45df01bd27a095dfe6e58f9994465b14fa10cc0
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2023-06-01 09:44:13 +0530

    Time: 87 ms (42.77%), Space: 19.3 MB (85.79%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,39 +1,31 @@
 class Solution {
+    vector<vector<int>> dirs = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+    int m, n;
 public:
-    // Helper function to check if point is in grid
-    bool isValidPoint(int x, int y, int n, int m) {
-        return x >= 0 && x <= n && y >= 0 && y <= m;
-    }
-    
-    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int n = grid.size()-1, m = grid[0].size()-1;
-        if (grid[0][0] || grid[n][m]) 
-            return -1;
+    int shortestPathBinaryMatrix(vector<vector<int>>& G) {
+        m = G.size(), n = G[0].size();
         
-        queue<vector<int> > q;
-        vector<int> curr;
+        if(G[0][0] || G[m-1][n-1]) return -1;
         
-        q.push({0, 0});
-        grid[0][0] = 1;
+        queue<pair<int, int>> q; q.push({0, 0});
+        G[0][0] = 1;
         
-        while (!q.empty() && !grid[n][m]) {
-            curr = q.front();
-            q.pop();
+        while(!q.empty() && !G[m-1][n-1]) {
+            auto curr = q.front(); q.pop();
             
-            for (int i = 0; i < 8; i++) {
-                int x = curr[0] + x_points[i];
-                int y = curr[1] + y_points[i];
+            for(int k=0; k<8; k++) {
+                int x = curr.first + dirs[k][0], y = curr.second + dirs[k][1];
                 
-                if (isValidPoint(x, y, n, m) && grid[x][y] == 0) {
-                    grid[x][y] = grid[curr[0]][curr[1]] + 1;
+                if(valid(G, x, y)) {
+                    G[x][y] = G[curr.first][curr.second] + 1;
                     q.push({x, y});
                 }
-            }
+            } 
         }
-        return grid[n][m] ? grid[n][m] : -1;
+        return G[m-1][n-1] ? G[m-1][n-1] : -1;
     }
     
-private:
-    vector<int> x_points = {-1,-1,-1,0,0,1,1,1};
-    vector<int> y_points = {-1,0,1,-1,1,-1,0,1};
+    bool valid(vector<vector<int>> &G, int x, int y) {
+        return x >= 0 && y >= 0 && x < m && y < n && G[x][y] == 0;
+    }
 };
