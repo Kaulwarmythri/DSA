227c77916948b24bfd00be6d3189ce1c9a9382f6
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2023-06-03 16:21:16 +0530

    Time: 484 ms (25.25%), Space: 162.5 MB (9.60%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
+    int numOfMinutes(int n, int hid, vector<int>& manager, vector<int>& informTime) {
         unordered_map<int,vector<int> >m;
+        
         for(int i=0;i<manager.size();i++) {
             m[manager[i]].push_back(i);
         }
-        queue<pair<int,int>>q;
-        q.push(make_pair(headID,0));
+        
+        queue<pair<int,int>>q; q.push({hid, 0});
         int maxi=0;
+        
         while(!q.empty()) {
-            int n=q.size();
-            for(int i=0;i<n;i++) {
-                int head=q.front().first;
-                int timeTaken=q.front().second;
-                q.pop();
-                int dur=informTime[head]+timeTaken;
-                maxi=max(maxi,dur);
-                for(auto it:m[head]) {
-                    q.push(make_pair(it,dur));
+            int s=q.size();
+            while(s--) {
+                auto [head, time] = q.front(); q.pop();
+                int dur = informTime[head] + time;
+                maxi = max(maxi, dur);
+                for(auto &it: m[head]) {
+                    q.push({it, dur});
                 }
             }
         }
