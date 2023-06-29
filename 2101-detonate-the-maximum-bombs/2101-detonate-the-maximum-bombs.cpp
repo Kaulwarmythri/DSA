3cf8cdcf6b86ba9dcf4911d6121844f32a818a5d
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2023-06-29 17:36:52 +0530

    Time: 109 ms (86.95%), Space: 14.9 MB (57.14%) - LeetHub

--- 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.cpp
+++ 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.cpp
@@ -1,43 +1,38 @@
-#define ll long long int
 class Solution {
-    public:
-    void dfs(vector<vector<int>> &gr,vector<bool> &visited,int &c,int &i) {
-        visited[i]=true;
-        c++;
-        for(int j=0;j<gr[i].size();j++) {
-            if(!visited[gr[i][j]])
-                dfs(gr,visited,c,gr[i][j]);   
-        }
-    }
-
-    int maximumDetonation(vector<vector<int>>& bombs) {
-
-        int n=bombs.size();
-        vector<vector<int> > gr(n);
-        for(int i=0;i<n;i++) {
-            ll x1,y1,r1;
-            x1=bombs[i][0];
-            y1=bombs[i][1];
-            r1=bombs[i][2];
-            for(int j=0;j<n;j++) {
-                if(i!=j) {
-                    ll x,y;
-                    x=abs(x1-bombs[j][0]);
-                    y=abs(y1-bombs[j][1]);
-                    if(x*x+y*y<=r1*r1)
-                        gr[i].push_back(j);
+public:
+    int maximumDetonation(vector<vector<int>>& B) {
+        int n = B.size(), ans = INT_MIN;
+        
+        vector<vector<int>> G(n);
+        
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                if(i != j) {
+                    long long x = abs(B[i][0] - B[j][0]), y = abs(B[i][1] - B[j][1]), r = B[i][2];
+                    
+                    if(x*x + y*y <= r*r) 
+                        G[i].push_back(j);
                 }
             }
         }
-
-        int ans=INT_MIN;
-        for(int i=0;i<n;i++) {
-            int c=0;
-            vector<bool> visited(n,false);
-            dfs(gr,visited,c,i);
-            ans=max(ans,c);
+        
+        for(int i=0; i<n; i++) {
+            vector<bool> vis(n);
+            int cur = 0;
+            
+            dfs(G, i, vis, cur);
+            ans = max(ans, cur);
         }
-
+        
         return ans;
+    }
+    
+    void dfs(vector<vector<int>> &G, int i, vector<bool> &vis, int &ans) {
+        vis[i] = true;
+        ans++;
+        
+        for(auto &j: G[i]) {
+            if(!vis[j]) dfs(G, j, vis, ans);
+        }
     }
 };
