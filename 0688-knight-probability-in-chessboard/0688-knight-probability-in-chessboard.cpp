bc9b7de588576759f6cec579124aaba1783bab15
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2023-07-22 08:55:47 +0530

    Time: 35 ms (29.70%), Space: 17.2 MB (19.26%) - LeetHub

--- 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.cpp
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.cpp
@@ -1,18 +1,21 @@
 class Solution {
-    int dir[8][2] = {{-1, -2}, {-2, -1}, {-1, 2}, {-2, 1}, {2, 1}, {1, 2}, {1, -2}, {2, -1}};
+    int dirs[8][2] = {{-2, -1}, {-1, -2}, {1, -2}, {2, -1}, {1, 2}, {2, 1}, {-2, 1}, {-1, 2}};
     double dp[26][26][1001] =  {[0 ... 25] = {[0 ... 25] = {[0 ... 100] = -1.0}}};
 public:
     double knightProbability(int n, int k, int i, int j) {
-        if(i < 0 || j < 0 || i >= n || j >= n) return 0.0;
+        if(i < 0 || j < 0 || i >= n || j >= n) 
+            return 0;
         
-        if(k == 0) return 1.0;
+        if(k == 0)
+            return 1;
         
-        if(dp[i][j][k] != -1) return dp[i][j][k];
+        if(dp[i][j][k] != -1.0)
+            return dp[i][j][k];
         
-        double ans = 0.0;
+        double sum = 0;
         for(int p=0; p<8; p++) {
-            ans += knightProbability(n, k-1, i+dir[p][0], j+dir[p][1]);
+            sum += knightProbability(n, k-1, i+dirs[p][0], j+dirs[p][1]);
         }
-        return dp[i][j][k] = ans / 8.0;
+        return dp[i][j][k] = sum / 8.0;
     }
 };
