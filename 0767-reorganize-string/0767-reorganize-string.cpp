6cb5b236dd84ca8c5c452653b101b2e9d440e018
Kaulwarmythri <63294858+Kaulwarmythri@users.noreply.github.com>
2023-08-23 23:27:47 +0530

    Time: 2 ms (56.28%), Space: 6.3 MB (46.07%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.cpp
+++ 0767-reorganize-string/0767-reorganize-string.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     string reorganizeString(string s) {
-        vector<int> cnt(26);
+        vector<int> count(26);
         int n = s.size(), mostFreq = 0, i = 0;
 
         for(auto &c : s)
-            if(++cnt[c - 'a'] > cnt[mostFreq])
-                mostFreq = (c - 'a');
+            if(++count[c - 'a'] > count[mostFreq])
+                mostFreq = c - 'a';
 
-        if(2 * cnt[mostFreq] - 1 > n) return "";
+        if(count[mostFreq] > (n + 1) / 2) return "";
 
-        while(cnt[mostFreq]) {
+        while(count[mostFreq]) {
             s[i] = ('a' + mostFreq);
             i += 2;
-            cnt[mostFreq]--;
+            count[mostFreq]--;
         }
 
         for(int j = 0; j < 26; j++) {
-            while(cnt[j]) {
+            while(count[j]) {
                 if(i >= n) i = 1;
                 s[i] = 'a' + j;
-                cnt[j]--;
+                count[j]--;
                 i += 2;
             }
         }
